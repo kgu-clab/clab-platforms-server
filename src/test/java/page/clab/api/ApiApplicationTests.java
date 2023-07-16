95718a87dbf773f5c31cf1e7f4824e1e4297bf92
limehee <noop103@naver.com>
2023-07-16 16:48:38 +0900

    Project Initiation

---
+++ src/test/java/page/clab/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package page.clab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
