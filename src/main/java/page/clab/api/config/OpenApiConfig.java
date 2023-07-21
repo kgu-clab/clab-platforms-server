54ed71e4e28187ca18f35c2ea8ded823c2a470b1
limehee <noop103@naver.com>
2023-07-21 10:54:11 +0900

    A: Swagger 추가

---
+++ src/main/java/page/clab/api/config/OpenApiConfig.java
@@ -0,0 +1,66 @@
+package page.clab.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        Info info = new Info().title("C-Lab Page").version(appVersion)
+                .description("C-Lab Page API Document")
+                .termsOfService("http://swagger.io/terms/")
+                .contact(new Contact().name("한관희").url("https://github.com/limehee").email("noop103@naver.com"))
+                .license(new License().name("C-Lab Page License Version 1.0").url("https://github.com/KGU-C-Lab/Clab-Server"));
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .info(info);
+    }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (operation, handlerMethod) -> {
+            if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+                Parameter parameter = new Parameter()
+                        .in("header")
+                        .name("Authorization")
+                        .schema(new io.swagger.v3.oas.models.media.Schema().type("string"));
+
+                operation.addParametersItem(parameter);
+            }
+            return operation;
+        };
+    }
+
+}
