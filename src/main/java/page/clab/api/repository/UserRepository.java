7f82f58f1873c310acbd67a6ffdb92a0a63fc2c2
LimeWindows <noop103@naver.com>
2023-07-25 22:01:05 +0900

    *: 코드 리팩토링

--- src/main/java/page/clab/api/repository/UserRepository.java
+++ src/main/java/page/clab/api/repository/UserRepository.java
@@ -3,9 +3,6 @@ package page.clab.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import page.clab.api.type.entity.User;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, String> {
 
-    Optional<User> getUserById(String userId);
 }
--- src/main/java/page/clab/api/type/dto/ApplicationRequestDto.java
+++ src/main/java/page/clab/api/type/dto/ApplicationRequestDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
