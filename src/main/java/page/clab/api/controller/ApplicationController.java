10ac536d0c2bf10a8e9966d8351bedd1aef0cd34
limehee <noop103@naver.com>
2023-07-25 18:23:36 +0900

    A: Application API 추가

--- src/main/java/page/clab/api/controller/ApplicationController.java
+++ src/main/java/page/clab/api/controller/ApplicationController.java
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import page.clab.api.service.ApplicationService;
 import page.clab.api.type.dto.ApplicationRequestDto;
+import page.clab.api.type.dto.ApplicationResponseDto;
 import page.clab.api.type.dto.ResponseModel;
-import page.clab.api.type.entity.Application;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -29,6 +31,7 @@ public class ApplicationController {
             "String email;<br>" +
             "String department;<br>" +
             "Long grade;<br>" +
+            "LocalDate birth;<br>" +
             "String address;<br>" +
             "String interests;<br>" +
             "String otherActivities;")
@@ -46,7 +49,21 @@ public class ApplicationController {
     public ResponseModel getAllApplication() {
 //        String userId = AuthUtil.getAuthenticationInfoUserId();
         String userId = "201912156"; // 임시 테스트용
-        List<Application> applications = applicationService.getAllApplication(userId);
+        List<ApplicationResponseDto> applications = applicationService.getAllApplication(userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(applications);
+        return responseModel;
+    }
+
+    @Operation(summary = "동아리 가입 신청자 목록 필터링(날짜 기준)", description = "전달된 날짜 사이의 신청자를 필터링함")
+    @GetMapping("/list")
+    public ResponseModel getApplicationsBetweenDates(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+    ) {
+//        String userId = AuthUtil.getAuthenticationInfoUserId();
+        String userId = "201912156"; // 임시 테스트용
+        List<ApplicationResponseDto> applications = applicationService.getApplicationsBetweenDates(startDate, endDate, userId);
         ResponseModel responseModel = ResponseModel.builder().build();
         responseModel.addData(applications);
         return responseModel;
@@ -59,9 +76,57 @@ public class ApplicationController {
     ) {
 //        String userId = AuthUtil.getAuthenticationInfoUserId();
         String userId = "201912156"; // 임시 테스트용
-        Application application = applicationService.getApplication(applicationId, userId);
+        ApplicationResponseDto application = applicationService.getApplicationById(applicationId, userId);
         ResponseModel responseModel = ResponseModel.builder().build();
         responseModel.addData(application);
+        return responseModel;
+    }
+
+    @Operation(summary = "동아리 가입 신청자 검색", description = "동아리 가입 신청자 검색(이름 기반)")
+    @GetMapping("/search")
+    public ResponseModel searchApplication(
+            @RequestParam String name
+    ) {
+//        String userId = AuthUtil.getAuthenticationInfoUserId();
+        String userId = "201912156"; // 임시 테스트용
+        ApplicationResponseDto application = applicationService.searchApplication(name, userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(application);
+        return responseModel;
+    }
+
+    @Operation(summary = "동아리 가입 신청 승인", description = "동아리 가입 신청 승인")
+    @PostMapping("/approve/{applicationId}")
+    public ResponseModel approveApplication(
+            @PathVariable String applicationId
+    ) {
+//        String userId = AuthUtil.getAuthenticationInfoUserId();
+        String userId = "201912156"; // 임시 테스트용
+        applicationService.approveApplication(applicationId, userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        return responseModel;
+    }
+
+    @Operation(summary = "동아리 가입 신청 취소", description = "동아리 가입 신청 취소 (24시간 내에만 가능)")
+    @PostMapping("/cancel/{applicationId}")
+    public ResponseModel cancelApplication(
+            @PathVariable String applicationId
+    ) {
+//        String userId = AuthUtil.getAuthenticationInfoUserId();
+        String userId = "201912156"; // 임시 테스트용
+        applicationService.cancelApplication(applicationId, userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        return responseModel;
+    }
+
+    @Operation(summary = "동아리 가입 합격자 목록", description = "동아리 가입 합격자 목록을 조회합니다.")
+    @GetMapping("/pass")
+    public ResponseModel getApprovedApplications() {
+//        String userId = AuthUtil.getAuthenticationInfoUserId();
+        String userId = "201912156"; // 임시 테스트용
+        List<ApplicationResponseDto> approvedApplications = applicationService.getApprovedApplications(userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(approvedApplications);
         return responseModel;
     }
 
--- src/main/java/page/clab/api/service/ApplicationService.java
+++ src/main/java/page/clab/api/service/ApplicationService.java
@@ -7,10 +7,14 @@ import page.clab.api.exception.NotFoundException;
 import page.clab.api.repository.ApplicationRepository;
 import page.clab.api.repository.UserRepository;
 import page.clab.api.type.dto.ApplicationRequestDto;
+import page.clab.api.type.dto.ApplicationResponseDto;
 import page.clab.api.type.entity.Application;
 import page.clab.api.type.entity.User;
 import page.clab.api.type.etc.Role;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,33 +26,99 @@ public class ApplicationService {
     private final UserRepository userRepository;
 
     public void createApplication(ApplicationRequestDto applicationRequestDto) {
-        Application application = Application.builder()
-                .studentId(applicationRequestDto.getStudentId())
-                .name(applicationRequestDto.getName())
-                .contact(applicationRequestDto.getContact())
-                .email(applicationRequestDto.getEmail())
-                .department(applicationRequestDto.getDepartment())
-                .grade(applicationRequestDto.getGrade())
-                .address(applicationRequestDto.getAddress())
-                .interests(applicationRequestDto.getInterests())
-                .otherActivities(applicationRequestDto.getOtherActivities())
-                .build();
+        Application application = Application.toApplication(applicationRequestDto);
         applicationRepository.save(application);
     }
 
-    public List<Application> getAllApplication(String userId) {
+    public List<ApplicationResponseDto> getAllApplication(String userId) {
         User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
         if (!user.getRole().equals(Role.ADMIN.getKey()))
             new UnAuthorizeException("권한이 부족합니다.");
         List<Application> applications = applicationRepository.findAll();
-        return applications;
+        List<ApplicationResponseDto> applicationResponseDtos = new ArrayList<>();
+        for (Application a : applications) {
+            ApplicationResponseDto applicationResponseDto = Application.toApplicationResponseDto(a);
+            if (userRepository.findById(a.getStudentId()).isPresent())
+                applicationResponseDto.setPass(true);
+            applicationResponseDtos.add(applicationResponseDto);
+        }
+        return applicationResponseDtos;
     }
 
-    public Application getApplication(String applicationId, String userId) {
+    public List<ApplicationResponseDto> getApplicationsBetweenDates(LocalDate startDate, LocalDate endDate, String userId) {
+        User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (!user.getRole().equals(Role.ADMIN.getKey()))
+            new UnAuthorizeException("권한이 부족합니다.");
+        LocalDateTime startDateTime = startDate.atStartOfDay();
+        LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
+        List<Application> applicationsBetweenDates = applicationRepository.findApplicationsBetweenDates(startDateTime, endDateTime);
+        List<ApplicationResponseDto> applicationResponseDtos = new ArrayList<>();
+        for (Application a : applicationsBetweenDates) {
+            ApplicationResponseDto applicationResponseDto = Application.toApplicationResponseDto(a);
+            if (userRepository.findById(a.getStudentId()).isPresent())
+                applicationResponseDto.setPass(true);
+            applicationResponseDtos.add(applicationResponseDto);
+        }
+        return applicationResponseDtos;
+    }
+
+    public ApplicationResponseDto getApplicationById(String applicationId, String userId) {
         User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
         if (!user.getRole().equals(Role.ADMIN.getKey()))
             new UnAuthorizeException("권한이 부족합니다.");
         Application application = applicationRepository.findById(applicationId).orElseThrow(() -> new NotFoundException("해당 신청자가 없습니다."));
-        return application;
+        ApplicationResponseDto applicationResponseDto = Application.toApplicationResponseDto(application);
+        if (userRepository.findById(applicationResponseDto.getStudentId()).isPresent())
+            applicationResponseDto.setPass(true);
+        return applicationResponseDto;
+    }
+
+    public List<ApplicationResponseDto> getApprovedApplications(String userId) {
+        User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (!user.getRole().equals(Role.ADMIN.getKey()))
+            new UnAuthorizeException("권한이 부족합니다.");
+        List<Application> applications = applicationRepository.findAll();
+        List<ApplicationResponseDto> applicationResponseDtos = new ArrayList<>();
+        for (Application a : applications) {
+            Application application = applicationRepository.findById(a.getStudentId()).orElseThrow(() -> new NotFoundException("해당 신청자가 없습니다."));
+            ApplicationResponseDto applicationResponseDto = Application.toApplicationResponseDto(a);
+            if (userRepository.findById(applicationResponseDto.getStudentId()).isPresent()) {
+                applicationResponseDto.setPass(true);
+                applicationResponseDtos.add(applicationResponseDto);
+            }
+        }
+        return applicationResponseDtos;
+    }
+
+    public ApplicationResponseDto searchApplication(String name, String userId) {
+        User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (!user.getRole().equals(Role.ADMIN.getKey()))
+            new UnAuthorizeException("권한이 부족합니다.");
+        Application application = applicationRepository.findByName(name).orElseThrow(() -> new NotFoundException("해당 신청자가 없습니다."));
+        ApplicationResponseDto applicationResponseDto = Application.toApplicationResponseDto(application);
+        if (userRepository.findById(applicationResponseDto.getStudentId()).isPresent())
+            applicationResponseDto.setPass(true);
+        return applicationResponseDto;
+    }
+
+    public void approveApplication(String applicationId, String userId) {
+        User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (!user.getRole().equals(Role.ADMIN.getKey()))
+            new UnAuthorizeException("권한이 부족합니다.");
+        Application application = applicationRepository.findById(applicationId).orElseThrow(() -> new NotFoundException("해당 신청자가 없습니다."));
+        User approvedUser = User.toUser(application);
+        userRepository.save(approvedUser);
+    }
+
+    public void cancelApplication(String applicationId, String userId) {
+        User user = userRepository.getUserById(userId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (!user.getRole().equals(Role.ADMIN.getKey()))
+            new UnAuthorizeException("권한이 부족합니다.");
+        Application application = applicationRepository.findById(applicationId).orElseThrow(() -> new NotFoundException("해당 신청자가 없습니다."));
+        User approvedUser = userRepository.findById(applicationId).orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
+        if (approvedUser.getCreatedAt().isBefore(LocalDateTime.now().minusDays(1))) {
+            throw new UnAuthorizeException("취소할 수 없는 신청입니다.");
+        }
+        userRepository.delete(approvedUser);
     }
 }
