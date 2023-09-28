47cb09cdfe3cfd4a8581ccd717a15687f101a739
limehee <noop103@naver.com>
2023-09-29 01:50:59 +0900

    *: 리팩토링

--- src/main/java/page/clab/api/controller/UserController.java
+++ src/main/java/page/clab/api/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -79,7 +80,7 @@ public class UserController {
             "String address;<br>" +
             "Boolean isInSchool<br>" +
             "String imageUrl;")
-    @PutMapping("/update")
+    @PatchMapping("")
     public ResponseModel updateUserInfoByUser(
             @RequestBody UpdateUserRequestDto updateUserRequestDto
     ) {
