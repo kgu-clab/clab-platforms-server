606c0b62800e024d49146aab46081fdbe53711ed
limehee <noop103@naver.com>
2023-09-29 01:39:41 +0900

    F: ModelMapper 리팩토링 및 버그 수정

--- src/main/java/page/clab/api/type/dto/UserRequestDto.java
+++ src/main/java/page/clab/api/type/dto/UserRequestDto.java
@@ -3,15 +3,17 @@ package page.clab.api.type.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.modelmapper.ModelMapper;
 import page.clab.api.type.entity.User;
+import page.clab.api.util.ModelMapperUtil;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserRequestDto {
 
@@ -35,10 +37,8 @@ public class UserRequestDto {
 
     private Boolean isInSchool;
 
-    private static ModelMapper modelMapper;
-
     public static UserRequestDto of(User user) {
-        return modelMapper.map(user, UserRequestDto.class);
+        return ModelMapperUtil.getModelMapper().map(user, UserRequestDto.class);
     }
 
 }
--- src/main/java/page/clab/api/type/entity/User.java
+++ src/main/java/page/clab/api/type/entity/User.java
@@ -3,16 +3,17 @@ package page.clab.api.type.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.URL;
-import org.modelmapper.ModelMapper;
 import org.springframework.format.annotation.DateTimeFormat;
 import page.clab.api.type.dto.UserRequestDto;
 import page.clab.api.type.dto.UserResponseDto;
 import page.clab.api.type.etc.OAuthProvider;
 import page.clab.api.type.etc.Role;
+import page.clab.api.util.ModelMapperUtil;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,9 +27,11 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -92,14 +95,29 @@ public class User {
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    private static ModelMapper modelMapper = new ModelMapper();
-
     public static User of(UserRequestDto userRequestDto) {
-        return modelMapper.map(userRequestDto, User.class);
+        return ModelMapperUtil.getModelMapper().map(userRequestDto, User.class);
     }
 
     public static User of(UserResponseDto userResponseDto) {
-        return modelMapper.map(userResponseDto, User.class);
+        return ModelMapperUtil.getModelMapper().map(userResponseDto, User.class);
+    }
+
+    public static User of(Application application) {
+        User user = ModelMapperUtil.getModelMapper().map(application, User.class);
+        user.setPassword(user.generatePassword(user.getBirth()));
+        user.setIsInSchool(true);
+        user.setRole(Role.USER);
+        user.setProvider(OAuthProvider.LOCAL);
+        return user;
+    }
+
+    public String generatePassword(LocalDate birthDate) {
+        if (birthDate == null) {
+            throw new IllegalArgumentException("생년월일이 올바르지 않습니다.");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+        return birthDate.format(formatter);
     }
 
 }
