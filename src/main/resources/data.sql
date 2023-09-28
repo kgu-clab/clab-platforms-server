2b4a2680fa4d4865563b26d36895265b7438ee2a
limehee <noop103@naver.com>
2023-09-29 00:31:31 +0900

    C: PostgreSQL 마이그레이션에 따른 쿼리문 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,13 @@
 -- User
-insert into user(id, password, uid, name, contact, email, department, grade, birth, address, is_in_school, image_url, role, provider)
-values ('201912156', '000527', '', '한관희', '010-5147-6788', 'noop103@naver.com', '컴퓨터공학부', 3, '2000-05-27', '수원시', true, '', 'ADMIN', 'LOCAL');
-insert into user(id, password, uid, name, contact, email, department, grade, birth, address, is_in_school, image_url, role, provider, created_at)
-values ('201912033', '000323', '', '김관식', '010-2134-4323', 'gwansik@naver.com', '컴퓨터공학부', 3, '2000-03-23', '수원시', true, '', 'USER', 'LOCAL', '2023-07-25 18:13:43.000000');
+INSERT INTO "user" (id, password, uid, name, contact, email, department, grade, birth, address, is_in_school, image_url, role, provider)
+VALUES ('201912156', '000527', '', '한관희', '010-5147-6788', 'noop103@naver.com', '컴퓨터공학부', 3, '2000-05-27', '수원시', true, '', 'ADMIN', 'LOCAL');
+
+INSERT INTO "user" (id, password, uid, name, contact, email, department, grade, birth, address, is_in_school, image_url, role, provider, created_at)
+VALUES ('201912033', '000323', '', '김관식', '010-2134-4323', 'gwansik@naver.com', '컴퓨터공학부', 3, '2000-03-23', '수원시', true, '', 'USER', 'LOCAL', '2023-07-25 18:13:43.000000');
 
 -- Application
-insert into application(student_id, name, contact, email, department, grade, birth, address, interests, other_activities, created_at)
-values ('201912033', '김관식', '010-2134-4323', 'gwansik@naver.com', '컴퓨터공학부', 3, '2000-03-23', '수원시', '', '', '2023-07-25 18:12:43.000000');
-insert into application(student_id, name, contact, email, department, grade, birth, address, interests, other_activities, created_at)
-values ('201912034', '박동민', '010-1234-5678', 'dongmin@naver.com', '컴퓨터공학부', 3, '2000-01-01', '수원시', '', '', '2022-07-25 18:12:48.000000');
+INSERT INTO "application" (student_id, name, contact, email, department, grade, birth, address, interests, other_activities, created_at)
+VALUES ('201912033', '김관식', '010-2134-4323', 'gwansik@naver.com', '컴퓨터공학부', 3, '2000-03-23', '수원시', '', '', '2023-07-25 18:12:43.000000');
+
+INSERT INTO "application" (student_id, name, contact, email, department, grade, birth, address, interests, other_activities, created_at)
+VALUES ('201912034', '박동민', '010-1234-5678', 'dongmin@naver.com', '컴퓨터공학부', 3, '2000-01-01', '수원시', '', '', '2022-07-25 18:12:48.000000');
