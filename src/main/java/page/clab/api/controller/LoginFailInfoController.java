7eaf2987de6b0dd913b4ff10f702f2b3e5d84752
limehee <noop103@naver.com>
2023-09-30 20:52:08 +0900

    *: LoginFailInfo 코드 분리

---
+++ src/main/java/page/clab/api/controller/LoginFailInfoController.java
@@ -0,0 +1,44 @@
+package page.clab.api.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import page.clab.api.exception.PermissionDeniedException;
+import page.clab.api.service.LoginFailInfoService;
+import page.clab.api.type.dto.ResponseModel;
+
+@RestController
+@RequestMapping("/login-fail-info")
+@RequiredArgsConstructor
+@Tag(name = "Login")
+@Slf4j
+public class LoginFailInfoController {
+
+    private final LoginFailInfoService loginFailInfoService;
+
+    @Operation(summary = "유저 밴 처리", description = "유저 밴 처리")
+    @PostMapping("/ban/{userId}")
+    public ResponseModel banUser(
+            @PathVariable String userId
+    ) throws PermissionDeniedException {
+        loginFailInfoService.banUserById(userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        return responseModel;
+    }
+
+    @Operation(summary = "유저 밴 해제", description = "유저 밴 해제")
+    @PostMapping("/unban/{userId}")
+    public ResponseModel unbanUser(
+            @PathVariable String userId
+    ) throws PermissionDeniedException {
+        loginFailInfoService.unbanUserById(userId);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        return responseModel;
+    }
+
+}
--- src/main/java/page/clab/api/service/LoginService.java
+++ src/main/java/page/clab/api/service/LoginService.java
@@ -10,18 +10,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import page.clab.api.auth.jwt.JwtTokenProvider;
 import page.clab.api.exception.LoginFaliedException;
-import page.clab.api.exception.NotFoundException;
-import page.clab.api.exception.PermissionDeniedException;
 import page.clab.api.exception.UserLockedException;
-import page.clab.api.repository.LoginFailInfoRepository;
 import page.clab.api.type.dto.RefreshTokenDto;
 import page.clab.api.type.dto.TokenDto;
 import page.clab.api.type.dto.TokenInfo;
 import page.clab.api.type.entity.LoginFailInfo;
 import page.clab.api.type.entity.User;
+import page.clab.api.type.etc.Role;
 
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -32,37 +29,28 @@ public class LoginService {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    private final LoginFailInfoRepository loginFailInfoRepository;
+    private final LoginFailInfoService loginFailInfoService;
 
     private final UserService userService;
-
-    private static final int MAX_LOGIN_FAILURES = 5;
-
-    private static final int LOCK_DURATION_MINUTES = 5;
 
     @Transactional
     public TokenInfo login(String id, String password) throws LoginFaliedException, UserLockedException {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(id, password);
         try {
             Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-            LoginFailInfo loginFailInfo = getLoginFailInfoByUserIdOrThrow(authentication.getName());
+            LoginFailInfo loginFailInfo = loginFailInfoService.getLoginFailInfoByUserIdOrThrow(authentication.getName());
             if (loginFailInfo == null) {
                 User user = userService.getUserByIdOrThrow(authentication.getName());
-                loginFailInfo = LoginFailInfo.builder()
-                        .user(user)
-                        .loginFailCount(0L)
-                        .isLock(false)
-                        .build();
-                loginFailInfoRepository.save(loginFailInfo);
+                loginFailInfo = loginFailInfoService.createLoginFailInfo(user);
             }
-            checkUserLocked(loginFailInfo);
-            resetLoginFailInfo(loginFailInfo);
+            loginFailInfoService.checkUserLocked(loginFailInfo);
+            loginFailInfoService.resetLoginFailInfo(loginFailInfo);
 
             TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
 
             return tokenInfo;
         } catch (BadCredentialsException e) {
-            updateLoginFailInfo(id);
+            loginFailInfoService.updateLoginFailInfo(id);
         }
 
         return null;
@@ -78,77 +66,15 @@ public class LoginService {
         return null;
     }
 
-    public void banUserById(String userId) throws PermissionDeniedException {
-        userService.checkUserAdminRole();
-        LoginFailInfo loginFailInfo = getLoginFailInfoByUserIdOrThrow(userId);
-        if (loginFailInfo != null) {
-            loginFailInfo.setIsLock(true);
-            loginFailInfo.setLatestTryLoginDate(LocalDateTime.now().plusYears(100));
-            loginFailInfoRepository.save(loginFailInfo);
-        }
-    }
-
-    public void unbanUserById(String userId) throws PermissionDeniedException {
-        userService.checkUserAdminRole();
-        LoginFailInfo loginFailInfo = getLoginFailInfoByUserIdOrThrow(userId);
-        if (loginFailInfo != null) {
-            loginFailInfo.setIsLock(false);
-            loginFailInfoRepository.save(loginFailInfo);
-        }
-    }
-
     public boolean checkTokenRole(TokenDto tokenDto) {
         String token = tokenDto.getToken();
         if (tokenDto != null && jwtTokenProvider.validateToken(token)) {
             Claims claims = jwtTokenProvider.parseClaims(token);
-            if (claims.get("role").toString().equals("ROLE_ADMIN")) {
+            if (claims.get("role").toString().equals(Role.ADMIN.getKey())) {
                 return true;
             }
         }
         return false;
-    }
-
-    private void checkUserLocked(LoginFailInfo loginFailInfo) throws UserLockedException {
-        if (loginFailInfo != null && loginFailInfo.getIsLock() && isLockedForDuration(loginFailInfo)) {
-            throw new UserLockedException();
-        }
-    }
-
-    private boolean isLockedForDuration(LoginFailInfo loginFailInfo) {
-        LocalDateTime unlockTime = loginFailInfo.getLatestTryLoginDate().plusMinutes(LOCK_DURATION_MINUTES);
-        return LocalDateTime.now().isBefore(unlockTime);
-    }
-
-    private void resetLoginFailInfo(LoginFailInfo loginFailInfo) {
-        if (loginFailInfo != null) {
-            loginFailInfo.setLoginFailCount(0L);
-            loginFailInfo.setIsLock(false);
-            loginFailInfoRepository.save(loginFailInfo);
-        }
-    }
-
-    private void updateLoginFailInfo(String userId) throws LoginFaliedException {
-        LoginFailInfo loginFailInfo = getLoginFailInfoByUserIdOrThrow(userId);
-        if (loginFailInfo != null) {
-            incrementFailCountAndLock(loginFailInfo);
-        }
-        throw new LoginFaliedException();
-    }
-
-    private void incrementFailCountAndLock(LoginFailInfo loginFailInfo) {
-        loginFailInfo.setLoginFailCount(loginFailInfo.getLoginFailCount() + 1);
-        if (loginFailInfo.getLoginFailCount() >= MAX_LOGIN_FAILURES) {
-            if (loginFailInfo.getIsLock().equals(false)) {
-                loginFailInfo.setLatestTryLoginDate(LocalDateTime.now());
-                loginFailInfo.setIsLock(true);
-            }
-        }
-        loginFailInfoRepository.save(loginFailInfo);
-    }
-
-    public LoginFailInfo getLoginFailInfoByUserIdOrThrow(String userId) {
-        return loginFailInfoRepository.findByUser_Id(userId)
-                .orElseThrow(() -> new NotFoundException("해당 유저가 없습니다."));
     }
 
 }
