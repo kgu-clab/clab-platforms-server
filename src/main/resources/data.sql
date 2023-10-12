7451fb11b6b1cbd2ff6089ba9faf164b80908076
LimeWindows <noop103@naver.com>
2023-10-12 23:05:29 +0900

    feat: 후원 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,9 +22,22 @@ VALUES ('201912034', '박동민', '01012345678', 'dongmin@naver.com', '컴퓨터
 -- INSERT INTO "board" (category, title, content, writer, created_at)
 -- VALUES ('졸업', '카카오 문열어', '<h1>네이버 흥해라</h1>', 'admin', '2022-07-25 18:12:43.000000');
 
+-- LoginFailInfo
 insert into login_fail_info(member_id, login_fail_count, is_lock)
 values ('admin', 0, false);
 insert into login_fail_info(member_id, login_fail_count, is_lock)
 values ('201912156', 0, false);
 insert into login_fail_info(member_id, login_fail_count, is_lock)
 values ('201912033', 0, false);
+
+-- Donation
+insert into donation(member_id, amount, message, created_at)
+values ('admin', 10000, '씨랩 화이팅', '2022-07-02 12:14:45.000000');
+insert into donation(member_id, amount, message, created_at)
+values ('admin', 53000, '씨랩 화이팅', '2022-07-15 07:43:12.000000');
+insert into donation(member_id, amount, message, created_at)
+values ('201912156', 32000, '씨랩 화이팅', '2022-08-02 09:46:00.000000');
+insert into donation(member_id, amount, message, created_at)
+values ('201912033', 150000, '씨랩 화이팅', '2022-09-26 18:32:00.000000');
+insert into donation(member_id, amount, message, created_at)
+values ('201912033', 256000, '씨랩 화이팅', '2022-10-12 23:59:03.000000');
