464707eaef877e62f6f6f5e644aa4af4dbc9fe05
limehee <noop103@naver.com>
2023-10-18 10:04:13 +0900

    feat: 공동계정 api 추가

---
+++ src/main/java/page/clab/api/type/dto/SharedAccountRequestDto.java
@@ -0,0 +1,24 @@
+package page.clab.api.type.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SharedAccountRequestDto {
+
+    private String username;
+
+    private String password;
+
+    private String platformName;
+
+    private String platformUrl;
+
+}
