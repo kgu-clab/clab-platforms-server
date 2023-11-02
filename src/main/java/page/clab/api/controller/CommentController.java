40b45db343eb6d85fdc69eac96091484695c5a2c
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-02 18:27:46 +0900

    feat : Comment 완료

--- src/main/java/page/clab/api/controller/CommentController.java
+++ src/main/java/page/clab/api/controller/CommentController.java
@@ -1,10 +1,22 @@
 package page.clab.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import page.clab.api.exception.PermissionDeniedException;
+import page.clab.api.service.CommentService;
+import page.clab.api.type.dto.CommentDto;
+import page.clab.api.type.dto.ResponseModel;
 
 @RestController
 @RequestMapping("/comments")
@@ -12,4 +24,54 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Comment")
 @Slf4j
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @Operation(summary = "댓글 생성", description = "댓글 생성<br> +" +
+            "String content;<br>" +
+            "String writer;<br>" +
+            "Long boardId;<br>" )
+    @PostMapping("")
+    public ResponseModel createComment(@RequestBody CommentDto commentDto) throws PermissionDeniedException {
+        commentService.createComment(commentDto);
+        return ResponseModel.builder().build();
+    }
+
+    @Operation(summary = "댓글 리스트 조회", description = "댓글 리스트 조회")
+    @GetMapping("")
+    public ResponseModel getComments(@RequestParam(required = false) Long boardId) {
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(commentService.getComments(boardId));
+        return responseModel;
+    }
+
+    @Operation(summary = "내 댓글 검색", description = "내 댓글 검색")
+    @GetMapping("/search")
+    public ResponseModel searchComment(
+            @RequestParam(required = false) Long memberId,
+            @RequestParam(required = false) String name
+    ) throws PermissionDeniedException {
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(commentService.searchComment(memberId, name));
+        return responseModel;
+    }
+
+    @Operation(summary = "댓글 수정", description = "댓글 수정<br> +" +
+            "String content;<br>" +
+            "String writer;<br>" +
+            "Long boardId;<br>" )
+    @PatchMapping("")
+    public ResponseModel updateComment(
+            @RequestParam Long commentId,
+            @RequestBody CommentDto commentDto) throws PermissionDeniedException {
+        commentService.updateComment(commentId, commentDto);
+        return ResponseModel.builder().build();
+    }
+
+    @Operation(summary = "댓글 삭제", description = "댓글 삭제")
+    @DeleteMapping("/{commentId}")
+    public ResponseModel deleteComment(@PathVariable("commentId") Long commentId) throws PermissionDeniedException {
+        commentService.deleteComment(commentId);
+        return ResponseModel.builder().build();
+    }
 }
--- src/main/java/page/clab/api/service/CommentService.java
+++ src/main/java/page/clab/api/service/CommentService.java
@@ -2,8 +2,94 @@ package page.clab.api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import page.clab.api.exception.NotFoundException;
+import page.clab.api.exception.PermissionDeniedException;
+import page.clab.api.repository.BoardRepository;
+import page.clab.api.repository.CommentRepository;
+import page.clab.api.type.dto.CommentDto;
+import page.clab.api.type.entity.Comment;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final BoardRepository boardRepository;
+
+    private final MemberService memberService;
+
+    private final EntityManager entityManager;
+
+    public void createComment(CommentDto commentDto) throws PermissionDeniedException {
+        memberService.checkMemberAdminRole();
+        boardRepository.findById(commentDto.getBoard().getId())
+                .orElseThrow(() -> new NotFoundException("게시글을 찾을 수 없습니다."));
+        Comment comment = Comment.of(commentDto);
+        commentRepository.save(comment);
+    }
+
+    public void updateComment(Long commentId, CommentDto commentDto) throws PermissionDeniedException {
+        memberService.checkMemberAdminRole();
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다."));
+        if (comment.getWriter() != memberService.getCurrentMember()){
+            throw new PermissionDeniedException("댓글 작성자만 수정할 수 있습니다.");
+        }
+        Comment updateComment = Comment.of(commentDto);
+        updateComment.setId(comment.getId());
+        updateComment.setBoard(comment.getBoard());
+        updateComment.setWriter(comment.getWriter());
+        updateComment.setUpdateTime(commentDto.getUpdateTime());
+        updateComment.setContent(commentDto.getContent());
+        commentRepository.save(updateComment);
+    }
+
+    public void deleteComment(Long commentId) throws PermissionDeniedException{
+        memberService.checkMemberAdminRole();
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundException("댓글을 찾을 수 없습니다."));
+        if (comment.getWriter() != memberService.getCurrentMember()) {
+            throw new PermissionDeniedException("댓글 작성자만 삭제할 수 있습니다.");
+        }
+        commentRepository.delete(comment);
+    }
+
+    public List<CommentDto> getComments(Long boardId) {
+        List<Comment> comments = commentRepository.findAllByBoardId(boardId);
+        return comments.stream()
+                .map(CommentDto::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<CommentDto> searchComment(Long memberId, String name) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Comment> criteriaQuery = criteriaBuilder.createQuery(Comment.class);
+        Root<Comment> commentRoot = criteriaQuery.from(Comment.class);
+
+        List<Predicate> predicates = new ArrayList<>();
+        if (memberId != null) {
+            predicates.add(criteriaBuilder.equal(commentRoot.get("memberId"), memberId));
+        }
+        if (name != null) {
+            predicates.add(criteriaBuilder.like(commentRoot.get("name"), "%" + name + "%"));
+        }
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+        TypedQuery<Comment> query = entityManager.createQuery(criteriaQuery);
+        List<Comment> comments = query.getResultList();
+        return comments.stream()
+                .map(CommentDto::of)
+                .collect(Collectors.toList());
+    }
+
 }
