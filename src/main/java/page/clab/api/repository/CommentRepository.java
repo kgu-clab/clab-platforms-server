65405f4ec69be2306a14b4ddf330ae69ac33b64a
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-02 18:27:46 +0900

    feat : Comment 완료

--- src/main/java/page/clab/api/repository/CommentRepository.java
+++ src/main/java/page/clab/api/repository/CommentRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import page.clab.api.type.entity.Comment;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByBoardId(Long boardId);
 }
