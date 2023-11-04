0d98dfa5b113ef53e30286d72b6cbdd6357851cf
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-04 11:23:15 +0900

    feat : 관리자 to 전체멤버 전송 기능 완성

--- src/main/java/page/clab/api/service/EmailService.java
+++ src/main/java/page/clab/api/service/EmailService.java
@@ -5,6 +5,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import page.clab.api.type.dto.EmailDto;
+import page.clab.api.type.dto.MemberResponseDto;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,12 +15,19 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendEmail(EmailDto emailDto) {
+    public void sendEmail(String to, EmailDto emailDto) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("gksrhksgml11@gmail.com");
-        message.setTo(emailDto.getTo());
+        message.setTo(to);
         message.setSubject(emailDto.getTitle());
         message.setText(emailDto.getContent());
         javaMailSender.send(message);
     }
+
+    public void broadcastEmailToAllMember(List<MemberResponseDto> memberList, EmailDto emailDto) {
+        for (MemberResponseDto member : memberList) {
+            sendEmail(member.getEmail(), emailDto);
+        }
+    }
+
 }
