f1d9f79672c54b38b5ff48808c474236fb1f4c1a
LimeWindows <noop103@naver.com>
2023-11-06 01:53:02 +0900

    refactor: List 정보를 추가하도록 변경

--- src/main/java/page/clab/api/service/ApplicationService.java
+++ src/main/java/page/clab/api/service/ApplicationService.java
@@ -70,7 +70,7 @@ public class ApplicationService {
         if (applicationId != null) {
             applications.add(getApplicationByIdOrThrow(applicationId));
         } else if (name != null) {
-            applications = getApplicationByName(name);
+            applications.addAll(getApplicationByName(name));
         } else {
             throw new IllegalArgumentException("적어도 applicationId, name 중 하나를 제공해야 합니다.");
         }
