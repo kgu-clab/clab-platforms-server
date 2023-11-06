59eb73938e5b62686953d9e833c2633518fd01b9
LimeWindows <noop103@naver.com>
2023-11-06 18:41:32 +0900

    feat: 바른 시스템 로그인 API를 이용하여 씨랩 페이지 로그인(기존 시스템 대체)

--- src/main/java/page/clab/api/service/LoginService.java
+++ src/main/java/page/clab/api/service/LoginService.java
@@ -1,13 +1,15 @@
 package page.clab.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
 import page.clab.api.auth.exception.TokenValidateException;
 import page.clab.api.auth.jwt.JwtTokenProvider;
 import page.clab.api.exception.LoginFaliedException;
@@ -16,18 +18,18 @@ import page.clab.api.type.dto.MemberLoginRequestDto;
 import page.clab.api.type.dto.RefreshTokenDto;
 import page.clab.api.type.dto.TokenDto;
 import page.clab.api.type.dto.TokenInfo;
+import page.clab.api.type.entity.Member;
 import page.clab.api.type.etc.LoginAttemptResult;
 import page.clab.api.type.etc.Role;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class LoginService {
-
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -41,20 +43,46 @@ public class LoginService {
     public TokenInfo login(HttpServletRequest httpServletRequest, MemberLoginRequestDto memberLoginRequestDto) throws LoginFaliedException, MemberLockedException {
         String id = memberLoginRequestDto.getId();
         String password = memberLoginRequestDto.getPassword();
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(id, password);
-        try {
-            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-            loginFailInfoService.handleLoginFailInfo(authentication.getName());
-            TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
-            memberService.setLastLoginTime(authentication.getName());
-            loginAttemptLogService.createLoginAttemptLog(httpServletRequest, authentication.getName(), LoginAttemptResult.SUCCESS);
-            return tokenInfo;
-        } catch (BadCredentialsException e) {
+        Member member = memberService.getMemberByIdOrThrow(id);
+        boolean loginSuccess = barunLogin(id, password);
+        TokenInfo tokenInfo = null;
+        if (loginSuccess) {
+            loginFailInfoService.handleLoginFailInfo(id);
+            tokenInfo = jwtTokenProvider.generateToken(id, member.getRole());
+            memberService.setLastLoginTime(id);
+            loginAttemptLogService.createLoginAttemptLog(httpServletRequest, id, LoginAttemptResult.SUCCESS);
+        } else {
             loginAttemptLogService.createLoginAttemptLog(httpServletRequest, id, LoginAttemptResult.FAILURE);
             loginFailInfoService.updateLoginFailInfo(id);
         }
-        loginAttemptLogService.createLoginAttemptLog(httpServletRequest, id, LoginAttemptResult.FAILURE);
-        return null;
+        return tokenInfo;
+    }
+
+    private boolean barunLogin(String id, String password) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("email", id);
+        formData.add("password", password);
+
+        WebClient webClient = WebClient.builder()
+                .baseUrl("https://barun.kyonggi.ac.kr")
+                .build();
+
+        String response = webClient
+                .post()
+                .uri("/ko/process/member/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue(formData)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> jsonResponse = objectMapper.readValue(response, Map.class);
+            return (boolean) jsonResponse.get("success");
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public TokenInfo reissue(RefreshTokenDto refreshTokenDto) {
