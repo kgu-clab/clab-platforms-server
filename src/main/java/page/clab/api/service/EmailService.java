3b8c66f9f7459dbd00f834befccbf3dbb32e1e92
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-06 09:25:51 +0900

    feat : 파일 업로드 기능 완성

--- src/main/java/page/clab/api/service/EmailService.java
+++ src/main/java/page/clab/api/service/EmailService.java
@@ -1,13 +1,17 @@
 package page.clab.api.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import page.clab.api.type.dto.EmailDto;
+import org.springframework.web.multipart.MultipartFile;
 import page.clab.api.type.dto.MemberResponseDto;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -15,18 +19,24 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendEmail(String to, EmailDto emailDto) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("gksrhksgml11@gmail.com");
-        message.setTo(to);
-        message.setSubject(emailDto.getTitle());
-        message.setText(emailDto.getContent());
+    public void sendEmail(String to, String subject, String content, MultipartFile file) throws MessagingException, IOException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+        messageHelper.setFrom("gksrhksgml11@gmail.com");
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(content);
+        if (file != null){
+            messageHelper.addAttachment(Objects.requireNonNull(file.getOriginalFilename()), file);
+        }
+
         javaMailSender.send(message);
     }
 
-    public void broadcastEmailToAllMember(List<MemberResponseDto> memberList, EmailDto emailDto) {
+    public void broadcastEmailToAllMember(List<MemberResponseDto> memberList, String subject, String content, MultipartFile file) throws MessagingException, IOException {
         for (MemberResponseDto member : memberList) {
-            sendEmail(member.getEmail(), emailDto);
+            sendEmail(member.getEmail(), subject, content, file);
         }
     }
 
