68a15c5e22c44eadee5bf2cb15bf79f58ba48a01
limehee <noop103@naver.com>
2023-11-07 20:28:08 +0900

    feat: 수상 이력 API 추가

---
+++ src/main/java/page/clab/api/type/dto/AwardRequestDto.java
@@ -0,0 +1,34 @@
+package page.clab.api.type.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AwardRequestDto {
+
+    @NotNull
+    private String competitionName;
+
+    @NotNull
+    private String organizer;
+
+    @NotNull
+    private String awardName;
+
+    @NotNull
+    private String participants;
+
+    @NotNull
+    private LocalDateTime awardDate;
+
+}
