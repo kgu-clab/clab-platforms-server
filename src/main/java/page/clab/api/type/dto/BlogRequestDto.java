fdc0f81190e5baa703119332b50f6b109765a03e
LimeWindows <noop103@naver.com>
2023-11-08 22:08:19 +0900

    feat: 블로그 API 추가

---
+++ src/main/java/page/clab/api/type/dto/BlogRequestDto.java
@@ -0,0 +1,28 @@
+package page.clab.api.type.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BlogRequestDto {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String subTitle;
+
+    @NotNull
+    private String content;
+
+    private String tag;
+
+}
