d66142399d8bf5a5da1143890c4db963077fcd4e
LimeWindows <noop103@naver.com>
2023-11-08 21:19:58 +0900

    feat: 경력사항 API 추가

---
+++ src/main/java/page/clab/api/service/WorkExperienceService.java
@@ -0,0 +1,71 @@
+package page.clab.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import page.clab.api.repository.WorkExperienceRepository;
+import page.clab.api.type.dto.WorkExperienceRequestDto;
+import page.clab.api.type.dto.WorkExperienceResponseDto;
+import page.clab.api.type.entity.Member;
+import page.clab.api.type.entity.WorkExperience;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class WorkExperienceService {
+
+    private final MemberService memberService;
+
+    private final WorkExperienceRepository workExperienceRepository;
+
+    public void createWorkExperience(WorkExperienceRequestDto workExperienceRequestDto) {
+        Member member = memberService.getCurrentMember();
+        WorkExperience workExperience = WorkExperience.of(workExperienceRequestDto);
+        workExperience.setMember(member);
+        workExperienceRepository.save(workExperience);
+    }
+
+    public List<WorkExperienceResponseDto> getMyWorkExperience() {
+        Member member = memberService.getCurrentMember();
+        List<WorkExperience> workExperiences = workExperienceRepository.findAllByMember_IdOrderByStartDateDesc(member.getId());
+        return workExperiences.stream()
+                .map(WorkExperienceResponseDto::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<WorkExperienceResponseDto> searchWorkExperience(String memberId) {
+        Member member = memberService.getMemberByIdOrThrow(memberId);
+        List<WorkExperience> workExperiences = workExperienceRepository.findAllByMember_IdOrderByStartDateDesc(member.getId());
+        return workExperiences.stream()
+                .map(WorkExperienceResponseDto::of)
+                .collect(Collectors.toList());
+    }
+
+    public void updateWorkExperience(Long workExperienceId, WorkExperienceRequestDto workExperienceRequestDto) {
+        Member member = memberService.getCurrentMember();
+        WorkExperience workExperience = getWorkExperienceByIdOrThrow(workExperienceId);
+        if (!(workExperience.getMember().getId().equals(member.getId()) || memberService.isMemberAdminRole(member))) {
+            throw new IllegalArgumentException("해당 경력사항을 수정할 권한이 없습니다.");
+        }
+        WorkExperience updatedWorkExperience = WorkExperience.of(workExperienceRequestDto);
+        updatedWorkExperience.setId(workExperienceId);
+        updatedWorkExperience.setMember(member);
+        workExperienceRepository.save(updatedWorkExperience);
+    }
+
+    public void deleteWorkExperience(Long workExperienceId) {
+        Member member = memberService.getCurrentMember();
+        WorkExperience workExperience = getWorkExperienceByIdOrThrow(workExperienceId);
+        if (!(workExperience.getMember().getId().equals(member.getId()) || memberService.isMemberAdminRole(member))) {
+            throw new IllegalArgumentException("해당 경력사항을 삭제할 권한이 없습니다.");
+        }
+        workExperienceRepository.deleteById(workExperienceId);
+    }
+
+    private WorkExperience getWorkExperienceByIdOrThrow(Long workExperienceId) {
+        return workExperienceRepository.findById(workExperienceId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 경력사항이 존재하지 않습니다."));
+    }
+
+}
---
+++ src/main/java/page/clab/api/type/entity/WorkExperience.java
@@ -0,0 +1,50 @@
+package page.clab.api.type.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import page.clab.api.type.dto.WorkExperienceRequestDto;
+import page.clab.api.util.ModelMapperUtil;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkExperience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @ManyToOne
+    private Member member;
+
+    public static WorkExperience of(WorkExperienceRequestDto workExperienceRequestDto) {
+        return ModelMapperUtil.getModelMapper().map(workExperienceRequestDto, WorkExperience.class);
+    }
+
+}
