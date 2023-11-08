6b4106852af1560dedeb42e43ad9eccc5f94ee10
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-08 16:16:14 +0900

    feat : ActivityGroupStatus 추가 및 dto 생성

---
+++ src/main/java/page/clab/api/type/dto/ActivityGroupDto.java
@@ -0,0 +1,32 @@
+package page.clab.api.type.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import page.clab.api.type.entity.ActivityGroup;
+import page.clab.api.util.ModelMapperUtil;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ActivityGroupDto {
+
+    private String category;
+
+    private String name;
+
+    private String content;
+
+    private String imageUrl;
+
+    private String schedule;
+
+    public static ActivityGroupDto of(ActivityGroup activityGroup) {
+        return ModelMapperUtil.getModelMapper().map(activityGroup, ActivityGroupDto.class);
+    }
+
+}
--- src/main/java/page/clab/api/type/entity/ActivityGroup.java
+++ src/main/java/page/clab/api/type/entity/ActivityGroup.java
@@ -7,9 +7,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.URL;
+import page.clab.api.type.dto.ActivityGroupDto;
+import page.clab.api.type.etc.ActivityGroupStatus;
+import page.clab.api.util.ModelMapperUtil;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -39,13 +44,26 @@ public class ActivityGroup {
     @Size(max = 1000)
     private String content;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ActivityGroupStatus status;
+
+    @Column
+    private Long progress;
+
+    @Column
+    private String code;
+
     @URL
     private String imageUrl;
 
-    private LocalDateTime schedule;
 
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    public static ActivityGroup of(ActivityGroupDto activityGroupDto) {
+        return ModelMapperUtil.getModelMapper().map(activityGroupDto, ActivityGroup.class);
+    }
 
 }
