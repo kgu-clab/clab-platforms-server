ab8660e3fd0303cb5cea19bfad446a5e530e388c
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-09 11:20:43 +0900

    refactor : Comment 수정 완료

--- src/main/java/page/clab/api/repository/BoardRepository.java
+++ src/main/java/page/clab/api/repository/BoardRepository.java
@@ -6,10 +6,12 @@ import page.clab.api.type.entity.Board;
 import page.clab.api.type.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    Optional<Board> findById(Long id);
     List<Board> findAllByWriter(Member writer);
 
     List<Board> findAllByCategory(String category);
--- src/main/java/page/clab/api/service/BoardService.java
+++ src/main/java/page/clab/api/service/BoardService.java
@@ -2,7 +2,6 @@ package page.clab.api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import page.clab.api.exception.NotFoundException;
 import page.clab.api.repository.BoardRepository;
 import page.clab.api.type.dto.BoardDto;
 import page.clab.api.type.entity.Board;
@@ -11,6 +10,7 @@ import page.clab.api.type.entity.Member;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +25,7 @@ public class BoardService {
     }
 
     public BoardDto getBoards(Long boardId) {
-        Board board = getBoardById(boardId);
+        Board board = getBoardById(boardId).orElseThrow();
         return BoardDto.of(board);
     }
 
@@ -60,7 +60,7 @@ public class BoardService {
     }
 
     public void updateBoard(Member member, Long boardId, BoardDto boardDto) {
-        Board board = getBoardById(boardId);
+        Board board = getBoardById(boardId).orElseThrow();
         if (board.getWriter() != member) {
             throw new IllegalArgumentException("작성자만 커뮤니티 글을 수정할 수 있습니다.");
         }
@@ -73,16 +73,15 @@ public class BoardService {
     }
 
     public void deleteBoard(Member member, Long boardId) {
-        Board board = getBoardById(boardId);
+        Board board = getBoardById(boardId).orElseThrow();
         if (!board.getWriter().getId().equals(member.getId())) {
             throw new IllegalArgumentException("작성자만 커뮤니티 글을 삭제할 수 있습니다.");
         }
         boardRepository.delete(board);
     }
 
-    public Board getBoardById(Long boardId) {
-        return boardRepository.findById(boardId)
-                .orElseThrow(() -> new NotFoundException("커뮤니티 글이 존재하지 않습니다."));
+    public Optional<Board> getBoardById(Long boardId) {
+        return boardRepository.findById(boardId);
     }
 
 }
