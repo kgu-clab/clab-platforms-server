fbee2f90fc476e84462c5af21c46f21b4299a931
LimeWindows <noop103@naver.com>
2023-11-09 17:46:39 +0900

    feat: 파일 업로드 시 이미지를 감지하여 압축하도록 함

--- src/main/java/page/clab/api/util/ImageCompressionUtil.java
+++ src/main/java/page/clab/api/util/ImageCompressionUtil.java
@@ -1,7 +1,9 @@
 package page.clab.api.util;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
+import page.clab.api.exception.ImageCompressionException;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
@@ -9,36 +11,20 @@ import javax.imageio.ImageWriter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Iterator;
 
 @Component
+@Slf4j
 public class ImageCompressionUtil {
-
-    private static String imageUrl;
-    private static String imageDirectory;
-
-    @Value("${resource.file.url}")
-    public void setImageUrl(String imageUrl) {
-        ImageCompressionUtil.imageUrl = imageUrl;
-    }
-
-    @Value("${resource.file.path}")
-    public void setImageDirectory(String imageDirectory) {
-        ImageCompressionUtil.imageDirectory = imageDirectory;
-    }
 
     public static void compressImage(String filePath, float quality) {
         try {
-            String inputImagePath = imageDirectory + extractFilePath(filePath);
-            System.out.println(inputImagePath);
-            File inputFile = new File(inputImagePath);
+            File inputFile = new File(filePath);
             BufferedImage image = ImageIO.read(inputFile);
-
             OutputStream os = new FileOutputStream(inputFile);
 
-            String formatName = getFileExtension(inputFile.getName());
+            String formatName = FilenameUtils.getExtension(inputFile.getName());
             Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(formatName);
             ImageWriter writer = writers.next();
 
@@ -50,25 +36,9 @@ public class ImageCompressionUtil {
             ImageIO.write(image, formatName, os);
 
             os.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new ImageCompressionException("이미지 압축 실패 : " + filePath);
         }
-    }
-
-    private static String extractFilePath(String imagePath) {
-        if (imagePath.startsWith(imageUrl)) {
-            return imagePath.substring(imageUrl.length());
-        } else {
-            return imagePath;
-        }
-    }
-
-    private static String getFileExtension(String filename) {
-        int dotIndex = filename.lastIndexOf('.');
-        if (dotIndex > 0 && dotIndex < filename.length() - 1) {
-            return filename.substring(dotIndex + 1).toLowerCase();
-        }
-        return "";
     }
 
 }
