66f2d14161dcf6aac37d2707fa01a4a88c372cd2
LimeWindows <noop103@naver.com>
2023-11-10 21:37:13 +0900

    feat: Login 유효성 검사 추가

--- src/main/java/page/clab/api/type/dto/TokenDto.java
+++ src/main/java/page/clab/api/type/dto/TokenDto.java
@@ -1,5 +1,7 @@
 package page.clab.api.type.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +15,8 @@ import lombok.Setter;
 @Builder
 public class TokenDto {
 
+    @NotNull(message = "{notNull.token.token}")
+    @Size(min = 1, message = "{size.token.token}")
     private String token;
 
 }
