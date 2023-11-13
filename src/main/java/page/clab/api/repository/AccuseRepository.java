f12b996e4f914cda2f8a58dc1c6afa36e7de41a0
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-13 22:20:51 +0900

    refactor : 신고사유 및 신고 내용 추가

--- src/main/java/page/clab/api/repository/AccuseRepository.java
+++ src/main/java/page/clab/api/repository/AccuseRepository.java
@@ -1,13 +1,19 @@
 package page.clab.api.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import page.clab.api.type.entity.Accuse;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccuseRepository extends JpaRepository<Accuse, Long> {
 
     Optional<Accuse> findByTargetId(Long targetId);
+
+    List<Accuse> findAllByTargetId(Long targetId);
+
+    List<Accuse> findAllByTargetIdAndCategory(Long targetId, String category);
     
 }
--- src/main/java/page/clab/api/service/AccuseService.java
+++ src/main/java/page/clab/api/service/AccuseService.java
@@ -1,14 +1,19 @@
 package page.clab.api.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import page.clab.api.exception.NotFoundException;
 import page.clab.api.repository.AccuseRepository;
-import page.clab.api.type.dto.AccuseDto;
+import page.clab.api.type.dto.AccuseRequestDto;
+import page.clab.api.type.dto.AccuseResponseDto;
 import page.clab.api.type.entity.Accuse;
+import page.clab.api.type.entity.Blog;
+import page.clab.api.type.entity.Board;
+import page.clab.api.type.entity.Comment;
+import page.clab.api.type.entity.News;
+import page.clab.api.type.entity.Review;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,72 +33,81 @@ public class AccuseService {
 
     private final AccuseRepository accuseRepository;
 
-    public List<AccuseDto> getAccuses() {
+    public List<AccuseResponseDto> getAccuses() {
         List<Accuse> accuses = accuseRepository.findAll();
         return accuses.stream()
-                .map(AccuseDto::of)
+                .map(AccuseResponseDto::of)
                 .collect(Collectors.toList());
     }
 
-    public void deleteAccuse(Long targetId) {
-        Accuse accuse = getAccuseByTargetIdOrThrow(targetId);
-        accuseRepository.delete(accuse);
+    public List<AccuseResponseDto> getAccuseByTargetId(Long targetId, String category) {
+        List<Accuse> accuses = findAllByTargetIdAndCategory(targetId, category);
+        return accuses.stream()
+                .map(AccuseResponseDto::of)
+                .collect(Collectors.toList());
     }
 
-    public void memberAccuse(String memberId) {
+    public void deleteAccuse(Long targetId, String category) {
+        List<Accuse> accuse = findAllByTargetIdAndCategory(targetId, category);
+        accuseRepository.deleteAll(accuse);
+    }
+
+    public void memberAccuse(String memberId, AccuseRequestDto accuseRequestDto) {
         memberService.getMemberByIdOrThrow(memberId);
-        Accuse accuse = checkIsAccused(Long.parseLong(memberId));
-        accuse.setCount(accuse.getCount() + 1);
+        Accuse accuse = Accuse.of(Long.parseLong(memberId));
+        accuse.setCategory("member");
+        accuse.setDescription(accuseRequestDto.getDescription());
         accuseRepository.save(accuse);
     }
 
-    public void boardAccuse(Long boardId) {
-        boardService.getBoardByIdOrThrow(boardId);
-        Accuse accuse = checkIsAccused(boardId);
-        accuse.setCount(accuse.getCount() + 1);
+    public void boardAccuse(Long boardId, AccuseRequestDto accuseRequestDto) {
+        Board board = boardService.getBoardByIdOrThrow(boardId);
+        Accuse accuse = Accuse.of(boardId);
+        accuse.setCategory("board");
+        accuse.setDescription(accuseRequestDto.getDescription());
+        accuse.setContent(board.getContent());
         accuseRepository.save(accuse);
     }
 
-    public void commentAccuse(Long commentId){
-        commentService.getCommentByIdOrThrow(commentId);
-        Accuse accuse = checkIsAccused(commentId);
-        accuse.setCount(accuse.getCount() + 1);
+    public void commentAccuse(Long commentId, AccuseRequestDto accuseRequestDto){
+        Comment comment = commentService.getCommentByIdOrThrow(commentId);
+        Accuse accuse = Accuse.of(commentId);
+        accuse.setCategory("comment");
+        accuse.setDescription(accuseRequestDto.getDescription());
+        accuse.setContent(comment.getContent());
         accuseRepository.save(accuse);
 
     }
 
-    public void blogAccuse(Long blogId){
-        blogService.getBlogByIdOrThrow(blogId);
-        Accuse accuse = checkIsAccused(blogId);
-        accuse.setCount(accuse.getCount() + 1);
+    public void blogAccuse(Long blogId, AccuseRequestDto accuseRequestDto){
+        Blog blog = blogService.getBlogByIdOrThrow(blogId);
+        Accuse accuse = Accuse.of(blogId);
+        accuse.setCategory("blog");
+        accuse.setDescription(accuseRequestDto.getDescription());
+        accuse.setContent(blog.getContent());
         accuseRepository.save(accuse);
     }
 
-    public void newsAccuse(Long newsId){
-        newsService.getNewsByIdOrThrow(newsId);
-        Accuse accuse = checkIsAccused(newsId);
-        accuse.setCount(accuse.getCount() + 1);
+    public void newsAccuse(Long newsId, AccuseRequestDto accuseRequestDto){
+        News news = newsService.getNewsByIdOrThrow(newsId);
+        Accuse accuse = Accuse.of(newsId);
+        accuse.setCategory("news");
+        accuse.setDescription(accuseRequestDto.getDescription());
+        accuse.setContent(news.getContent());
         accuseRepository.save(accuse);
     }
 
-    public void reviewAccuse(Long reviewId){
-        reviewService.getReviewByIdOrThrow(reviewId);
-        Accuse accuse = checkIsAccused(reviewId);
-        accuse.setCount(accuse.getCount() + 1);
+    public void reviewAccuse(Long reviewId, AccuseRequestDto accuseRequestDto){
+        Review review = reviewService.getReviewByIdOrThrow(reviewId);
+        Accuse accuse = Accuse.of(reviewId);
+        accuse.setCategory("review");
+        accuse.setDescription(accuseRequestDto.getDescription());
+        accuse.setContent(review.getContent());
         accuseRepository.save(accuse);
     }
 
-    private Accuse getAccuseByTargetIdOrThrow(Long id) {
-        return accuseRepository.findByTargetId(id)
-                .orElseThrow(() -> new NotFoundException("해당 신고 내역이 존재하지 않습니다."));
-    }
-
-    private Accuse checkIsAccused(Long targetId){
-        Accuse accuse = accuseRepository.findByTargetId(targetId).orElse(null);
-        if (accuse == null) {
-            return Accuse.of(targetId);
-        }
-        return accuse;
+    private List<Accuse> findAllByTargetIdAndCategory(Long targetId, String category) {
+        return accuseRepository.findAllByTargetIdAndCategory(targetId, category);
     }
 
 }
