8f9d8670ea20d5dfdc148c9309233166017ad234
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-13 14:30:45 +0900

    feat : 멤버 신고 api 완성

--- src/main/java/page/clab/api/repository/AccuseRepository.java
+++ src/main/java/page/clab/api/repository/AccuseRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import page.clab.api.type.entity.Accuse;
 
+import java.util.Optional;
+
 @Repository
 public interface AccuseRepository extends JpaRepository<Accuse, Long> {
 
+    Optional<Accuse> findByTargetId(Long targetId);
 }
