0a25ce7f8c1896acf1fefd2e61e9f3176eda4e51
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-25 16:37:42 +0900

    feat : 활동 및 그룹 멤버 엔티티 수정

--- src/main/java/page/clab/api/type/entity/GroupMember.java
+++ src/main/java/page/clab/api/type/entity/GroupMember.java
@@ -1,12 +1,5 @@
 package page.clab.api.type.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +7,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import page.clab.api.type.dto.GroupMemberDto;
 import page.clab.api.type.etc.ActivityGroupRole;
+import page.clab.api.type.etc.GroupMemberStatus;
 import page.clab.api.util.ModelMapperUtil;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -37,6 +40,10 @@ public class GroupMember {
 
     @Enumerated(EnumType.STRING)
     private ActivityGroupRole role;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private GroupMemberStatus status;
 
     public static GroupMember of(GroupMemberDto groupMemberDto) {
         return ModelMapperUtil.getModelMapper().map(groupMemberDto, GroupMember.class);
