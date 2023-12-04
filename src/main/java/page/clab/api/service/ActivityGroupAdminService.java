e9d83a0798a369beeaf6f203448eea655d021f89
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-12-04 16:20:01 +0900

    feat : AccuseService 알림 설정

--- src/main/java/page/clab/api/service/ActivityGroupAdminService.java
+++ src/main/java/page/clab/api/service/ActivityGroupAdminService.java
@@ -1,7 +1,5 @@
 package page.clab.api.service;
 
-import java.util.List;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +20,9 @@ import page.clab.api.type.entity.Member;
 import page.clab.api.type.etc.ActivityGroupRole;
 import page.clab.api.type.etc.ActivityGroupStatus;
 import page.clab.api.type.etc.GroupMemberStatus;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
