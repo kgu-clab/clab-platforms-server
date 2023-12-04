775f8cec4281fa3e6e65ade89bd1f6ff972d6aae
LimeWindows <noop103@naver.com>
2023-12-05 02:36:41 +0900

    refactor: SecretKey을 직접 반환하도록 변경 및 미사용 코드 제거

--- src/main/java/page/clab/api/service/AuthenticatorService.java
+++ src/main/java/page/clab/api/service/AuthenticatorService.java
@@ -2,14 +2,12 @@ package page.clab.api.service;
 
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
-import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import page.clab.api.exception.NotFoundException;
 import page.clab.api.repository.AuthenticatorRepository;
 import page.clab.api.type.entity.Authenticator;
-
-// ... (다른 import문들은 그대로 유지)
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +15,6 @@ import page.clab.api.type.entity.Authenticator;
 public class AuthenticatorService {
 
     private final AuthenticatorRepository authenticatorRepository;
-
-    private final String issuer = "CLAB";
 
     public String generateSecretKey(String memberId) {
         GoogleAuthenticator gAuth = new GoogleAuthenticator();
@@ -32,21 +28,6 @@ public class AuthenticatorService {
         return secretKey;
     }
 
-    public String generateTotp(String memberId) {
-        GoogleAuthenticator gAuth = new GoogleAuthenticator();
-        Authenticator authenticator = authenticatorRepository.getById(memberId);
-        String secretKey = authenticator.getSecretKey();
-        int totp = gAuth.getTotpPassword(secretKey);
-        return String.format("%06d", totp);
-    }
-
-    public String generateSecretKeyQRCodeUrl(String accountName) {
-        Authenticator authenticator = authenticatorRepository.getById(accountName);
-        String secretKey = authenticator.getSecretKey();
-        GoogleAuthenticatorKey googleAuthenticatorKey = new GoogleAuthenticatorKey.Builder(secretKey).build();
-        return GoogleAuthenticatorQRGenerator.getOtpAuthURL(issuer, accountName, googleAuthenticatorKey);
-    }
-
     public boolean isAuthenticatorValid(String memberId, String totp) {
         return isAuthenticatorExist(memberId) && validateTotp(memberId, totp);
     }
@@ -58,12 +39,13 @@ public class AuthenticatorService {
         return gAuth.authorize(secretKey, Integer.parseInt(totp));
     }
 
-    public boolean isAuthenticatorExist(String memberId) {
-        return authenticatorRepository.existsById(memberId);
+    public Authenticator getAuthenticatorById(String memberId) {
+        return authenticatorRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException("해당 아이디에 대한 정보가 존재하지 않습니다."));
     }
 
-    public void deleteAuthenticator(String memberId) {
-        authenticatorRepository.deleteById(memberId);
+    public boolean isAuthenticatorExist(String memberId) {
+        return authenticatorRepository.existsById(memberId);
     }
 
 }
