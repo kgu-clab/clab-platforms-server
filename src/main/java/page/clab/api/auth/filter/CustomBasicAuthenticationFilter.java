30fa49b9c7ceeed9d0cf9e074127079ac3f66836
LimeWindows <noop103@naver.com>
2023-12-06 03:31:43 +0900

    fix: Swagger 외의 엔드포인트에서 basic auth 팝업이 뜨는 오류 수정

---
+++ src/main/java/page/clab/api/auth/filter/CustomBasicAuthenticationFilter.java
@@ -0,0 +1,79 @@
+package page.clab.api.auth.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.regex.Pattern;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class CustomBasicAuthenticationFilter extends BasicAuthenticationFilter {
+
+    private static final String[] SWAGGER_PATTERNS = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/.*",
+            "/swagger-ui.html",
+            "/v3/api-docs/.*",
+            "/swagger-ui/.*"
+    };
+
+    public CustomBasicAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String path = ((HttpServletRequest) request).getRequestURI();
+        boolean isSwagger = false;
+        for (String pattern : SWAGGER_PATTERNS) {
+            if (Pattern.compile(pattern).matcher(path).find()) {
+                isSwagger = true;
+                break;
+            }
+        }
+        if (!isSwagger) {
+            chain.doFilter(request, response);
+            return;
+        }
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Basic ")) {
+            response.setHeader("WWW-Authenticate", "Basic realm=\"Please enter your username and password\"");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return;
+        }
+
+        String base64Credentials = authorizationHeader.substring("Basic ".length());
+        String credentials = new String(Base64.getDecoder().decode(base64Credentials));
+        String[] values = credentials.split(":", 2);
+
+        if (values.length < 2) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid basic authentication token");
+            return;
+        }
+
+        String username = values[0];
+        String password = values[1];
+
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authentication = getAuthenticationManager().authenticate(authRequest);
+
+        if (authentication == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid username or password");
+            return;
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        super.doFilterInternal(request, response, chain);
+    }
+
+}
