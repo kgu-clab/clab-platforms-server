3d31c0c34a13d81b711f460aa4f162f34c4334e8
LimeWindows <noop103@naver.com>
2024-01-13 23:46:56 +0900

    feat: 공동 계정 이용 관련 API 추가

---
+++ src/main/java/page/clab/api/type/dto/SharedAccountUsageResponseDto.java
@@ -0,0 +1,41 @@
+package page.clab.api.type.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import page.clab.api.type.entity.SharedAccountUsage;
+import page.clab.api.type.etc.SharedAccountUsageStatus;
+import page.clab.api.util.ModelMapperUtil;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SharedAccountUsageResponseDto {
+
+    private Long id;
+
+    private String username;
+
+    private String platformName;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private String memberId;
+
+    private SharedAccountUsageStatus status;
+
+    public static SharedAccountUsageResponseDto of(SharedAccountUsage sharedAccountUsage) {
+        SharedAccountUsageResponseDto sharedAccountUsageResponseDto = ModelMapperUtil.getModelMapper().map(sharedAccountUsage, SharedAccountUsageResponseDto.class);
+        sharedAccountUsageResponseDto.setUsername(sharedAccountUsage.getSharedAccount().getUsername());
+        sharedAccountUsageResponseDto.setPlatformName(sharedAccountUsage.getSharedAccount().getPlatformName());
+        return sharedAccountUsageResponseDto;
+    }
+
+}
