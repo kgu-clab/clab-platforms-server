ea95848311dadd4f454a58e21a73f64b0707d53d
mingmingmon <mingmingmon@naver.com>
2024-01-16 00:26:09 +0900

    refactor(AbsentRequestDto): NotNull 유효성 검사 메세지 추가, LocalDate 사용

--- src/main/java/page/clab/api/type/dto/AbsentRequestDto.java
+++ src/main/java/page/clab/api/type/dto/AbsentRequestDto.java
@@ -1,6 +1,7 @@
 package page.clab.api.type.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,21 +18,21 @@ import page.clab.api.util.ModelMapperUtil;
 @Builder
 public class AbsentRequestDto {
 
-    @NotNull
+    @NotNull(message = "{notNull.absent.absenteeId}")
     @Schema(description = "불참 학생 학번", example = "202311220", required = true)
     private String absenteeId;
 
-    @NotNull
+    @NotNull(message = "{notNull.absent.activityGroupId}")
     @Schema(description = "불참 그룹 아이디", example = "1", required = true)
     private Long activityGroupId;
 
-    @NotNull
+    @NotNull(message = "{notNull.absent.reason}")
     @Schema(description = "불참 사유", example = "독감", required = true)
     private String reason;
 
-    @NotNull
+    @NotNull(message = "{notNull.absent.absentDate}")
     @Schema(description = "불참 날짜", example = "2023-11-12", required = true)
-    private String absentDate;
+    private LocalDate absentDate;
 
     public static AbsentRequestDto of(Absent absent){
         return ModelMapperUtil.getModelMapper().map(absent, AbsentRequestDto.class);
