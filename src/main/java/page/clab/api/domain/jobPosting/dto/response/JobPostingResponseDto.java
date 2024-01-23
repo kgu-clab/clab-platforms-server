eb34bdb094ec5025b6a91ef5dc96a864688153eb
LimeWindows <noop103@naver.com>
2024-01-24 01:10:40 +0900

    feat: 채용 공고 API 추가(CRUD)

--- src/main/java/page/clab/api/domain/jobPosting/dto/response/JobPostingResponseDto.java
+++ src/main/java/page/clab/api/domain/jobPosting/dto/response/JobPostingResponseDto.java
@@ -1,13 +1,11 @@
 package page.clab.api.domain.jobPosting.dto.response;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import page.clab.api.domain.jobPosting.domain.CareerLevel;
-import page.clab.api.domain.jobPosting.domain.EmploymentType;
 import page.clab.api.domain.jobPosting.domain.JobPosting;
 import page.clab.api.global.util.ModelMapperUtil;
 
@@ -22,20 +20,18 @@ public class JobPostingResponseDto {
     
     private String title;
 
-    private CareerLevel careerLevel;
-
-    private EmploymentType employmentType;
-
-    private String companyName;
-
     private String recruitmentPeriod;
 
     private String jobPostingUrl;
     
-    private LocalDateTime createdAt;
-
     public static JobPostingResponseDto of(JobPosting jobPosting) {
         return ModelMapperUtil.getModelMapper().map(jobPosting, JobPostingResponseDto.class);
+    }
+
+    public static List<JobPostingResponseDto> of(List<JobPosting> jobPostingList) {
+        return jobPostingList.stream()
+                .map(JobPostingResponseDto::of)
+                .toList();
     }
 
 }
