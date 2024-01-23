8bf7a9a123a678893381e4b7fdfc1537ccc6c44f
LimeWindows <noop103@naver.com>
2024-01-24 00:21:52 +0900

    feat: 채용 공고 Entity, Dto 추가

---
+++ src/main/java/page/clab/api/domain/jobPosting/domain/JobPosting.java
@@ -0,0 +1,54 @@
+package page.clab.api.domain.jobPosting.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(indexes = {@Index(name = "idx_job_posting_url", columnList = "jobPostingUrl")})
+public class JobPosting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(min = 1, max = 100, message = "{size.jobPosting.title}")
+    private String title;
+
+    private CareerLevel careerLevel;
+
+    private EmploymentType employmentType;
+
+    @Column(nullable = false)
+    @Size(min = 1, message = "{size.jobPosting.companyName}")
+    private String companyName;
+
+    private String recruitmentPeriod;
+
+    @URL(message = "{url.jobPosting.jobPostingUrl}")
+    private String jobPostingUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+}
