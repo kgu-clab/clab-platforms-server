41407a78d12ddefa140b209edad571df033d5e35
LimeWindows <noop103@naver.com>
2024-01-26 15:29:03 +0900

    refactor: CareerLevel에 무관(NOT_SPECIFIED) 추가

--- src/main/java/page/clab/api/domain/jobPosting/domain/CareerLevel.java
+++ src/main/java/page/clab/api/domain/jobPosting/domain/CareerLevel.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum CareerLevel {
 
     FRESHMAN("FRESHMAN", "신입"),
-    EXPERIENCED("EXPERIENCED", "경력");
+    EXPERIENCED("EXPERIENCED", "경력"),
+    NOT_SPECIFIED("NOT_SPECIFIED", "무관");
 
     private final String key;
     private final String description;
