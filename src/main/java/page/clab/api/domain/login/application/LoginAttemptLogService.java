1ce8ab508893eb8f5c034fc09d08609e3dc849cd
LimeWindows <noop103@naver.com>
2024-01-31 01:09:26 +0900

    refactor: LoginAttemptLog에서 Member 외래키 설정 제거

--- src/main/java/page/clab/api/domain/login/application/LoginAttemptLogService.java
+++ src/main/java/page/clab/api/domain/login/application/LoginAttemptLogService.java
@@ -11,8 +11,6 @@ import page.clab.api.domain.login.domain.GeoIpInfo;
 import page.clab.api.domain.login.domain.LoginAttemptLog;
 import page.clab.api.domain.login.domain.LoginAttemptResult;
 import page.clab.api.domain.login.dto.response.LoginAttemptLogResponseDto;
-import page.clab.api.domain.member.application.MemberService;
-import page.clab.api.domain.member.domain.Member;
 import page.clab.api.global.common.dto.PagedResponseDto;
 import page.clab.api.global.util.GeoIpUtil;
 import page.clab.api.global.util.HttpReqResUtil;
@@ -23,13 +21,11 @@ public class LoginAttemptLogService {
 
     private final LoginAttemptLogRepository loginAttemptLogRepository;
 
-    private final MemberService memberService;
-
     public void createLoginAttemptLog(HttpServletRequest httpServletRequest, String memberId, LoginAttemptResult loginAttemptResult) {
         String clientIpAddress = HttpReqResUtil.getClientIpAddressIfServletRequestExist();
         GeoIpInfo geoIpInfo = GeoIpUtil.getInfoByIp(clientIpAddress);
         LoginAttemptLog loginAttemptLog = LoginAttemptLog.builder()
-                .member(memberService.getMemberById(memberId))
+                .memberId(memberId)
                 .userAgent(httpServletRequest.getHeader("User-Agent"))
                 .ipAddress(clientIpAddress)
                 .location(geoIpInfo.getLocation())
@@ -40,13 +36,12 @@ public class LoginAttemptLogService {
     }
 
     public PagedResponseDto<LoginAttemptLogResponseDto> getLoginAttemptLogs(String memberId, Pageable pageable) {
-        Member member = memberService.getMemberByIdOrThrow(memberId);
-        Page<LoginAttemptLog> loginAttemptLogs = getLoginAttemptByMember(pageable, member);
+        Page<LoginAttemptLog> loginAttemptLogs = getLoginAttemptByMemberId(pageable, memberId);
         return new PagedResponseDto<>(loginAttemptLogs.map(LoginAttemptLogResponseDto::of));
     }
 
-    private Page<LoginAttemptLog> getLoginAttemptByMember(Pageable pageable, Member member) {
-        return loginAttemptLogRepository.findAllByMemberOrderByLoginAttemptTimeDesc(member, pageable);
+    private Page<LoginAttemptLog> getLoginAttemptByMemberId(Pageable pageable, String memberId) {
+        return loginAttemptLogRepository.findAllByMemberIdOrderByLoginAttemptTimeDesc(memberId, pageable);
     }
 
 }
