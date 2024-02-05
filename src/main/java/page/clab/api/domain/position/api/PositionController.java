674788c3d956fb9e8a21c7270cb8caa294ea2a90
limehee <noop103@naver.com>
2024-02-05 18:58:26 +0900

    refactor: 운영진 관련 API -> 직책 관련 API 변경

--- src/main/java/page/clab/api/domain/position/api/PositionController.java
+++ src/main/java/page/clab/api/domain/position/api/PositionController.java
@@ -19,72 +19,75 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import page.clab.api.domain.position.application.PositionService;
+import page.clab.api.domain.position.domain.PositionType;
 import page.clab.api.domain.position.dto.request.PositionRequestDto;
+import page.clab.api.domain.position.dto.response.PositionMyResponseDto;
 import page.clab.api.domain.position.dto.response.PositionResponseDto;
 import page.clab.api.global.common.dto.PagedResponseDto;
 import page.clab.api.global.common.dto.ResponseModel;
 
 @RestController
-@RequestMapping("/executives")
+@RequestMapping("/positions")
 @RequiredArgsConstructor
-@Tag(name = "Executive", description = "역대 운영진 관련 API")
+@Tag(name = "Position", description = "멤버 직책 관련 API")
 @Slf4j
 public class PositionController {
 
     private final PositionService positionService;
 
-    @Operation(summary = "운영진 등록", description = "ROLE_ADMIN 이상의 권한이 필요함")
+    @Operation(summary = "직책 등록", description = "ROLE_ADMIN 이상의 권한이 필요함")
     @Secured({"ROLE_ADMIN", "ROLE_SUPER"})
     @PostMapping("")
-    public ResponseModel createExecutive(
+    public ResponseModel createPosition(
             @Valid @RequestBody PositionRequestDto positionRequestDto,
             BindingResult result
     ) throws MethodArgumentNotValidException {
         if (result.hasErrors()) {
             throw new MethodArgumentNotValidException(null, result);
         }
-        Long id = positionService.createExecutive(positionRequestDto);
+        Long id = positionService.createPosition(positionRequestDto);
         ResponseModel responseModel = ResponseModel.builder().build();
         responseModel.addData(id);
         return responseModel;
     }
 
-    @Operation(summary = "역대 운영진 목록 조회", description = "ROLE_USER 이상의 권한이 필요함")
+    @Operation(summary = "연도/직책별 목록 조회", description = "ROLE_USER 이상의 권한이 필요함<br>" +
+            "연도와 직책을 선택하여 조회할 수 있음<br>" +
+            "연도와 직책을 선택하지 않으면 전체 목록을 조회함")
     @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_SUPER"})
     @GetMapping("")
-    public ResponseModel getExecutives(
+    public ResponseModel searchPositions(
+            @RequestParam(name = "year", required = false) String year,
+            @RequestParam(name = "positionType", required = false) PositionType positionType,
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "size", defaultValue = "20") int size
     ) {
         Pageable pageable = PageRequest.of(page, size);
-        PagedResponseDto<PositionResponseDto> executives = positionService.getExecutives(pageable);
+        PagedResponseDto<PositionResponseDto> positions = positionService.searchPositions(year, positionType, pageable);
         ResponseModel responseModel = ResponseModel.builder().build();
-        responseModel.addData(executives);
+        responseModel.addData(positions);
         return responseModel;
     }
 
-    @Operation(summary = "연도별 운영진 목록 조회", description = "ROLE_USER 이상의 권한이 필요함")
+    @Operation(summary = "나의 직책 조회", description = "ROLE_USER 이상의 권한이 필요함")
     @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_SUPER"})
-    @GetMapping("/{year}")
-    public ResponseModel getExecutivesByYear(
-            @PathVariable(name = "year") String year,
-            @RequestParam(name = "page", defaultValue = "0") int page,
-            @RequestParam(name = "size", defaultValue = "20") int size
+    @GetMapping("/my-positions")
+    public ResponseModel getMyPositionsByYear(
+            @RequestParam(name = "year", required = false) String year
     ) {
-        Pageable pageable = PageRequest.of(page, size);
-        PagedResponseDto<PositionResponseDto> executives = positionService.getExecutivesByYear(pageable, year);
+        PositionMyResponseDto positions = positionService.getMyPositionsByYear(year);
         ResponseModel responseModel = ResponseModel.builder().build();
-        responseModel.addData(executives);
+        responseModel.addData(positions);
         return responseModel;
     }
 
-    @Operation(summary = "역대 운영진 삭제", description = "ROLE_ADMIN 이상의 권한이 필요함")
+    @Operation(summary = "직책 삭제", description = "ROLE_ADMIN 이상의 권한이 필요함")
     @Secured({"ROLE_ADMIN", "ROLE_SUPER"})
-    @DeleteMapping("/{executiveId}")
-    public ResponseModel deleteExecutive(
-            @PathVariable(name = "executiveId") Long executiveId
+    @DeleteMapping("/{positionId}")
+    public ResponseModel deletePosition(
+            @PathVariable("positionId") Long positionId
     ) {
-        Long id = positionService.deleteExecutive(executiveId);
+        Long id = positionService.deletePosition(positionId);
         ResponseModel responseModel = ResponseModel.builder().build();
         responseModel.addData(id);
         return responseModel;
--- src/main/java/page/clab/api/domain/position/dao/PositionRepository.java
+++ src/main/java/page/clab/api/domain/position/dao/PositionRepository.java
@@ -3,14 +3,25 @@ package page.clab.api.domain.position.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import page.clab.api.domain.member.domain.Member;
 import page.clab.api.domain.position.domain.Position;
+import page.clab.api.domain.position.domain.PositionType;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PositionRepository extends JpaRepository<Position, Long> {
 
-    @Query("SELECT e FROM Position e ORDER BY e.year DESC, e.positionType ASC")
-    Page<Position> findAllByOrderByYearDescPositionAsc(Pageable pageable);
+    Optional<Position> findByMemberAndYearAndPositionType(Member member, String year, PositionType positionType);
 
-    Page<Position> findAllByYearOrderByPositionAsc(String year, Pageable pageable);
+    Page<Position> findAllByOrderByYearDescPositionTypeAsc(Pageable pageable);
+
+    Page<Position> findAllByYearOrderByPositionTypeAsc(String year, Pageable pageable);
+
+    Page<Position> findAllByYearAndPositionTypeOrderByPositionTypeAsc(String year, PositionType positionType, Pageable pageable);
+
+    Page<Position> findAllByPositionTypeOrderByYearDescPositionTypeAsc(PositionType positionType, Pageable pageable);
+
+    List<Position> findAllByMemberAndYearOrderByPositionTypeAsc(Member member, String year);
 
 }
