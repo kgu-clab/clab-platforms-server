7888005b3477eac19e332373a111a4ffa283c1c3
LimeWindows <noop103@naver.com>
2024-02-06 02:31:14 +0900

    refactor: API 요청시 로그에 사용자 IP와 ID를 같이 남기도록 변경

--- src/main/java/page/clab/api/global/handler/ApiLoggingInterceptor.java
+++ src/main/java/page/clab/api/global/handler/ApiLoggingInterceptor.java
@@ -3,9 +3,12 @@ package page.clab.api.global.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import page.clab.api.global.util.HttpReqResUtil;
 
 @Component
 @Slf4j
@@ -19,6 +22,9 @@ public class ApiLoggingInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = (authentication == null || authentication.getName() == null) ? "anonymous" : authentication.getName();
+        String clientIpAddress = HttpReqResUtil.getClientIpAddressIfServletRequestExist();
         String requestUrl = request.getRequestURI();
         String httpMethod = request.getMethod();
         int httpStatus = response.getStatus();
@@ -27,6 +33,6 @@ public class ApiLoggingInterceptor implements HandlerInterceptor {
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
 
-        log.info("[{}] {} {} {}ms", requestUrl, httpMethod, httpStatus, duration);
+        log.info("[{}:{}] {} {} {} {}ms", clientIpAddress, id, requestUrl, httpMethod, httpStatus, duration);
     }
 }
