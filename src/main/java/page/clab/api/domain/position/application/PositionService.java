f75ffabc8a965addb831d82f34459cab2a1dd2a3
limehee <noop103@naver.com>
2024-02-05 18:58:26 +0900

    refactor: 운영진 관련 API -> 직책 관련 API 변경

--- src/main/java/page/clab/api/domain/position/application/PositionService.java
+++ src/main/java/page/clab/api/domain/position/application/PositionService.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import page.clab.api.domain.position.dao.PositionRepository;
-import page.clab.api.domain.position.domain.Position;
-import page.clab.api.domain.position.dto.request.PositionRequestDto;
-import page.clab.api.domain.position.dto.response.PositionResponseDto;
 import page.clab.api.domain.member.application.MemberService;
 import page.clab.api.domain.member.domain.Member;
+import page.clab.api.domain.position.dao.PositionRepository;
+import page.clab.api.domain.position.domain.Position;
+import page.clab.api.domain.position.domain.PositionType;
+import page.clab.api.domain.position.dto.request.PositionRequestDto;
+import page.clab.api.domain.position.dto.response.PositionMyResponseDto;
+import page.clab.api.domain.position.dto.response.PositionResponseDto;
 import page.clab.api.global.common.dto.PagedResponseDto;
 import page.clab.api.global.exception.NotFoundException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,35 +25,69 @@ public class PositionService {
 
     private final PositionRepository positionRepository;
 
-    public Long createExecutive(PositionRequestDto positionRequestDto) {
-        Member executiveMember = memberService.getMemberByIdOrThrow(positionRequestDto.getMemberId());
-        Position position = Position.of(positionRequestDto);
-        return positionRepository.save(position).getId();
+    public Long createPosition(PositionRequestDto positionRequestDto) {
+        Member member = memberService.getMemberByIdOrThrow(positionRequestDto.getMemberId());
+        Position position = getPositionByMemberAndYearAndPositionType(positionRequestDto, member);
+        if (position == null) {
+            position = Position.of(positionRequestDto);
+            return positionRepository.save(position).getId();
+        }
+        return position.getId();
     }
 
-    public PagedResponseDto<PositionResponseDto> getExecutives(Pageable pageable) {
-        Page<Position> executives = positionRepository.findAllByOrderByYearDescPositionAsc(pageable);
-        return new PagedResponseDto<>(executives.map(PositionResponseDto::of));
+    public PagedResponseDto<PositionResponseDto> searchPositions(String year, PositionType positionType, Pageable pageable) {
+        Page<Position> positions = null;
+        if (year != null && positionType != null) {
+            positions = getPositionsByYearAndPositionTypeOrderByPositionTypeAsc(year, positionType, pageable);
+        } else if (year != null) {
+            positions = getPositionsByYearOrderByPositionTypeAsc(year, pageable);
+        } else if (positionType != null) {
+            positions = getPositionsByPositionTypeOrderByYearDescPositionTypeAsc(positionType, pageable);
+        } else {
+            positions = getPositionsByOrderByYearDescPositionTypeAsc(pageable);
+        }
+        return new PagedResponseDto<>(positions.map(PositionResponseDto::of));
     }
 
-    public PagedResponseDto<PositionResponseDto> getExecutivesByYear(Pageable pageable, String year) {
-        Page<Position> executives = getExecutivesByYear(year, pageable);
-        return new PagedResponseDto<>(executives.map(PositionResponseDto::of));
+    public PositionMyResponseDto getMyPositionsByYear(String year) {
+        Member member = memberService.getCurrentMember();
+        List<Position> positions = getPositionsByMemberAndYear(member, year);
+        return PositionMyResponseDto.of(positions);
     }
 
-    public Long deleteExecutive(Long executivesId) {
-        Position position = getExecutivesByIdOrThrow(executivesId);
+    public Long deletePosition(Long positionId) {
+        Position position = getPositionsByIdOrThrow(positionId);
         positionRepository.delete(position);
         return position.getId();
     }
 
-    private Position getExecutivesByIdOrThrow(Long executivesId) {
-        return positionRepository.findById(executivesId)
+    private Position getPositionsByIdOrThrow(Long positionId) {
+        return positionRepository.findById(positionId)
                 .orElseThrow(() -> new NotFoundException("해당 운영진이 존재하지 않습니다."));
     }
 
-    private Page<Position> getExecutivesByYear(String year, Pageable pageable) {
-        return positionRepository.findAllByYearOrderByPositionAsc(year, pageable);
+    private Page<Position> getPositionsByYearAndPositionTypeOrderByPositionTypeAsc(String year, PositionType positionType, Pageable pageable) {
+        return positionRepository.findAllByYearAndPositionTypeOrderByPositionTypeAsc(year, positionType, pageable);
+    }
+
+    private Page<Position> getPositionsByYearOrderByPositionTypeAsc(String year, Pageable pageable) {
+        return positionRepository.findAllByYearOrderByPositionTypeAsc(year, pageable);
+    }
+
+    private Page<Position> getPositionsByPositionTypeOrderByYearDescPositionTypeAsc(PositionType positionType, Pageable pageable) {
+        return positionRepository.findAllByPositionTypeOrderByYearDescPositionTypeAsc(positionType, pageable);
+    }
+
+    private Page<Position> getPositionsByOrderByYearDescPositionTypeAsc(Pageable pageable) {
+        return positionRepository.findAllByOrderByYearDescPositionTypeAsc(pageable);
+    }
+
+    private Position getPositionByMemberAndYearAndPositionType(PositionRequestDto positionRequestDto, Member member) {
+        return positionRepository.findByMemberAndYearAndPositionType(member, positionRequestDto.getYear(), positionRequestDto.getPositionType()).orElse(null);
+    }
+
+    private List<Position> getPositionsByMemberAndYear(Member member, String year) {
+        return positionRepository.findAllByMemberAndYearOrderByPositionTypeAsc(member, year);
     }
 
 }
