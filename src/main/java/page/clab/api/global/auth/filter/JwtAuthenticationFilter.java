bc77a9fa69e472d5de1c29c6c4759f5ad383a1a4
limehee <noop103@naver.com>
2024-02-10 08:18:37 +0900

    refactor: conflict 해결 중 import 소실

--- src/main/java/page/clab/api/global/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/page/clab/api/global/auth/filter/JwtAuthenticationFilter.java
@@ -5,9 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -15,11 +12,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 import page.clab.api.domain.blacklistIp.dao.BlacklistIpRepository;
 import page.clab.api.domain.login.application.RedisTokenService;
-import page.clab.api.domain.login.domain.RedisToken;
 import page.clab.api.global.auth.application.RedisIpAttemptService;
 import page.clab.api.global.auth.jwt.JwtTokenProvider;
-import page.clab.api.global.common.dto.ResponseModel;
-import page.clab.api.global.util.HttpReqResUtil;
+import page.clab.api.global.common.slack.application.SlackService;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @Slf4j
