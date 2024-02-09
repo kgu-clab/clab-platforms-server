9ab62ac756b08fbb968d4c3c23a6c0466c7ec523
mingmingmon <mingmingmon@naver.com>
2024-02-09 23:28:35 +0900

    feat: 비관적 락을 사용한 동시성 처리 #132

--- src/main/java/page/clab/api/domain/board/dao/BoardLikeRepository.java
+++ src/main/java/page/clab/api/domain/board/dao/BoardLikeRepository.java
@@ -1,12 +1,15 @@
 package page.clab.api.domain.board.dao;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import page.clab.api.domain.board.domain.Board;
 import page.clab.api.domain.board.domain.BoardLike;
 import page.clab.api.domain.member.domain.Member;
 
 public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     BoardLike findByBoardIdAndMemberId(Long boardId, String memberId);
 
     boolean existsByBoardIdAndMemberId(Long boardId, String memberId);
--- src/main/java/page/clab/api/domain/comment/dao/CommentLikeRepository.java
+++ src/main/java/page/clab/api/domain/comment/dao/CommentLikeRepository.java
@@ -1,6 +1,8 @@
 package page.clab.api.domain.comment.dao;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import page.clab.api.domain.board.domain.Board;
 import page.clab.api.domain.board.domain.BoardLike;
@@ -10,6 +12,8 @@ import page.clab.api.domain.member.domain.Member;
 
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     CommentLike findByCommentIdAndMemberId(Long commentId, String memberId);
 
     boolean existsByCommentIdAndMemberId(Long commentId, String memberId);
