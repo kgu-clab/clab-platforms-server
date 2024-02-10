523f2f2b00c78dfb012699aea9353d5a133b4483
limehee <noop103@naver.com>
2024-02-10 16:50:58 +0900

    feat(LoginFailInfo): 밴 유저 조회 API 추가

--- src/main/java/page/clab/api/domain/login/api/LoginFailInfoController.java
+++ src/main/java/page/clab/api/domain/login/api/LoginFailInfoController.java
@@ -4,12 +4,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import page.clab.api.domain.login.application.LoginFailInfoService;
+import page.clab.api.domain.login.dto.response.LoginFailInfoResponseDto;
+import page.clab.api.global.common.dto.PagedResponseDto;
 import page.clab.api.global.common.dto.ResponseModel;
 
 @RestController
@@ -42,6 +48,20 @@ public class LoginFailInfoController {
         Long id = loginFailInfoService.unbanMemberById(memberId);
         ResponseModel responseModel = ResponseModel.builder().build();
         responseModel.addData(id);
+        return responseModel;
+    }
+
+    @Operation(summary = "[S] 밴 유저 조회", description = "ROLE_SUPER 이상의 권한이 필요함")
+    @Secured({"ROLE_SUPER"})
+    @GetMapping("")
+    public ResponseModel getBanList(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "20") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        PagedResponseDto<LoginFailInfoResponseDto> loginFailInfoResponseDtos = loginFailInfoService.getBanList(pageable);
+        ResponseModel responseModel = ResponseModel.builder().build();
+        responseModel.addData(loginFailInfoResponseDtos);
         return responseModel;
     }
 
