7233e818123fb97a66e56ed825b0e875fe06bcfe
limehee <noop103@naver.com>
2024-02-10 11:17:23 +0900

    refactor(Donation): 후원 정보 수정/삭제 권한을 SUPER로 축소

--- src/main/java/page/clab/api/domain/donation/api/DonationController.java
+++ src/main/java/page/clab/api/domain/donation/api/DonationController.java
@@ -96,7 +96,8 @@ public class DonationController {
         return responseModel;
     }
 
-    @Operation(summary = "[U] 후원 정보 수정", description = "ROLE_USER 이상의 권한이 필요함")
+    @Operation(summary = "[U] 후원 정보 수정", description = "ROLE_USER 이상의 권한이 필요함<br>" +
+            "본인 외의 정보는 ROLE_SUPER만 가능")
     @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_SUPER"})
     @PatchMapping("/{donationId}")
     public ResponseModel updateDonation(
@@ -113,7 +114,8 @@ public class DonationController {
         return responseModel;
     }
 
-    @Operation(summary = "[U] 후원 삭제", description = "ROLE_USER 이상의 권한이 필요함")
+    @Operation(summary = "[U] 후원 삭제", description = "ROLE_USER 이상의 권한이 필요함<br>" +
+            "본인 외의 정보는 ROLE_SUPER만 가능")
     @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_SUPER"})
     @DeleteMapping("/{donationId}")
     public ResponseModel deleteDonation(
