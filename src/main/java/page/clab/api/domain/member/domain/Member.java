d784551801825ce1b8d4ea3f457e5d70f53eb4d4
limehee <noop103@naver.com>
2024-02-10 16:02:38 +0900

    refactor(Member): OAuth 미사용으로 인한 OAuthProvider 제거

--- src/main/java/page/clab/api/domain/member/domain/Member.java
+++ src/main/java/page/clab/api/domain/member/domain/Member.java
@@ -10,10 +10,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Collections;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,6 +22,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import page.clab.api.domain.member.dto.request.MemberRequestDto;
 import page.clab.api.global.util.ModelMapperUtil;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
@@ -92,9 +93,6 @@ public class Member implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Enumerated(EnumType.STRING)
-    private OAuthProvider provider;
-
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
@@ -142,7 +140,6 @@ public class Member implements UserDetails {
         Member member = ModelMapperUtil.getModelMapper().map(memberRequestDto, Member.class);
         member.setMemberStatus(MemberStatus.ACTIVE);
         member.setRole(Role.USER);
-        member.setProvider(OAuthProvider.LOCAL);
         return member;
     }
 
