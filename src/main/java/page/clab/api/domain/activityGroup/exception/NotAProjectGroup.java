85b839c04e4ff14f6541fb4f5c0d091f302a8379
mingmingmon <mingmingmon@naver.com>
2024-02-12 21:29:24 +0900

    fix: NotAProjectGroup, NotAStudyGroup 예외 추가 후 500이 아닌 400번대 에러 반환 #189

---
+++ src/main/java/page/clab/api/domain/activityGroup/exception/NotAProjectGroup.java
@@ -0,0 +1,9 @@
+package page.clab.api.domain.activityGroup.exception;
+
+public class NotAProjectGroup extends RuntimeException {
+
+    public NotAProjectGroup(String message) {
+        super(message);
+    }
+
+}
