10ad2053707655c68ca73a15e6db4a4ed37dfde4
mingmingmon <mingmingmon@naver.com>
2024-02-14 02:18:26 +0900

    chore: import문 최적화 #191

--- src/main/java/page/clab/api/global/common/file/api/FileController.java
+++ src/main/java/page/clab/api/global/common/file/api/FileController.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
