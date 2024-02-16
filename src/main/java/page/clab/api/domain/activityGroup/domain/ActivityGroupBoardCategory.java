903586a01f57ae05a3eb2b3d0248a1fc4a5ee262
mingmingmon <mingmingmon@naver.com>
2024-02-16 23:24:39 +0900

    refactor(ActivityGroupBoardCategory) : 카테고리 enum 수정 #213

--- src/main/java/page/clab/api/domain/activityGroup/domain/ActivityGroupBoardCategory.java
+++ src/main/java/page/clab/api/domain/activityGroup/domain/ActivityGroupBoardCategory.java
@@ -10,7 +10,8 @@ public enum ActivityGroupBoardCategory {
     NOTICE("NOTICE", "공지사항"),
     WEEKLY_ACTIVITY("WEEKLY_ACTIVITY", "주차별활동"),
     FEEDBACK("FEEDBACK", "피드백"),
-    ASSIGNMENT("ASSIGNMENT", "과제제출");
+    ASSIGNMENT("ASSIGNMENT", "과제"),
+    SUBMIT("SUBMIT", "제출");
 
     private String key;
     private String description;
