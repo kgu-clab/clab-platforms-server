9ed4432e9c3289151314cfc7085acfb040ae2a84
mingmingmon <mingmingmon@naver.com>
2024-02-17 15:29:26 +0900

    feat: 파일 업로드 api 수정에 따른 Board dto와 api 수정 #212

--- src/main/java/page/clab/api/domain/board/domain/Board.java
+++ src/main/java/page/clab/api/domain/board/domain/Board.java
@@ -2,13 +2,17 @@ package page.clab.api.domain.board.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +21,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import page.clab.api.domain.board.dto.request.BoardRequestDto;
 import page.clab.api.domain.member.domain.Member;
+import page.clab.api.global.common.file.domain.UploadedFile;
 import page.clab.api.global.util.ModelMapperUtil;
 
 @Entity
@@ -49,6 +54,10 @@ public class Board {
     @Column(nullable = false, length = 10000)
     @Size(min = 1, max = 10000, message = "{size.board.content}")
     private String content;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "files")
+    private List<UploadedFile> uploadedFiles = new ArrayList<>();
 
     @Column(name = "update_time")
     private LocalDateTime updateTime;
