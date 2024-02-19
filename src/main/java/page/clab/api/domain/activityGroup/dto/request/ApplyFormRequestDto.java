114d3b8c9a349b72101cceacd6d9f59ee06ada54
limehee <noop103@naver.com>
2024-02-19 22:49:15 +0900

    refactor(ActivityGroup): 활동 신청시 지원 동기만 입력받도록 변경

--- src/main/java/page/clab/api/domain/activityGroup/dto/request/ApplyFormRequestDto.java
+++ src/main/java/page/clab/api/domain/activityGroup/dto/request/ApplyFormRequestDto.java
@@ -15,22 +15,6 @@ import lombok.Setter;
 @Builder
 public class ApplyFormRequestDto {
 
-    @NotNull(message = "{notnull.applyForm.applierName}")
-    @Schema(description = "지원자 이름", example = "홍길동")
-    private String applierName;
-
-    @NotNull(message = "{notnull.applyForm.applierId}")
-    @Schema(description = "지원자 학번", example = "202311011")
-    private String applierId;
-
-    @NotNull(message = "{notnull.applyForm.applierDepartment}")
-    @Schema(description = "지원자 학과", example = "컴퓨터공학전공")
-    private String applierDepartment;
-
-    @NotNull(message = "{notnull.applyForm.applierYear}")
-    @Schema(description = "지원자 학년", example = "3")
-    private String applierYear;
-
     @NotNull(message = "{notnull.applyForm.applyReason}")
     @Schema(description = "지원 동기", example = "백엔드에 관심이 있어서")
     private String applyReason;
