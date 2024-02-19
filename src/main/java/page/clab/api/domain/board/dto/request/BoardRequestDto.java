89957dac6aa964f0498b0979bc9ad79ef257359a
limehee <noop103@naver.com>
2024-02-20 01:10:59 +0900

    refactor(File): 파일 경로 리스트의 스키마 예제 값을 리스트로 변경

--- src/main/java/page/clab/api/domain/board/dto/request/BoardRequestDto.java
+++ src/main/java/page/clab/api/domain/board/dto/request/BoardRequestDto.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +11,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import page.clab.api.domain.board.domain.Board;
 import page.clab.api.global.util.ModelMapperUtil;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -35,7 +36,7 @@ public class BoardRequestDto {
     @Schema(description = "내용", example = "2023년 2학기 모집 안내", required = true)
     private String content;
 
-    @Schema(description = "첨부파일 경로 리스트", example = "/resources/files/boards/339609571877700_4305d83e-090a-480b-a470-b5e96164d113.png")
+    @Schema(description = "첨부파일 경로 리스트", example = "[\"/resources/files/boards/339609571877700_4305d83e-090a-480b-a470-b5e96164d113.png\", \"/resources/files/boards/4305d83e-090a-480b-a470-b5e96164d114.png\"]")
     private List<String> fileUrlList;
 
     @NotNull(message = "{notNull.board.wantAnonymous}")
