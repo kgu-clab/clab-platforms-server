911f15a667f36cf142b5cdd1c71ba5d70d80de5d
limehee <noop103@naver.com>
2024-02-28 01:26:21 +0900

    feat(IpAccessMonitor): 비정상 접근으로 잠긴 IP 조회 및 삭제 API 추가

--- src/main/java/page/clab/api/global/common/slack/domain/SecurityAlertType.java
+++ src/main/java/page/clab/api/global/common/slack/domain/SecurityAlertType.java
@@ -13,6 +13,7 @@ public enum SecurityAlertType {
     UNAUTHORIZED_ACCESS("인가되지 않은 접근", "Unauthorized access attempt."),
     BLACKLISTED_IP_ADDED("블랙리스트 IP 등록", "IP address has been added to the blacklist."),
     BLACKLISTED_IP_REMOVED("블랙리스트 IP 해제", "IP address has been removed from the blacklist."),
+    ABNORMAL_ACCESS_IP_DELETED("비정상적인 접근 IP 삭제", "Abnormal access IP has been deleted."),
     MEMBER_BANNED("멤버 밴 등록", "Member has been banned."),
     MEMBER_UNBANNED("멤버 밴 해제", "Member has been unbanned.");
 
