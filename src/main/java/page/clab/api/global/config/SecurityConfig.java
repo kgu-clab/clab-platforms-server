16db4e0af7a375aa7a6929360a6e29c259d8f62d
mingmingmon <mingmingmon@naver.com>
2024-02-27 23:54:40 +0900

    refactor(SecurityConfig): 로그인 시 아이디 틀릴 경우 UserNotFoundException 발생 시키고 401 반환 #245

--- src/main/java/page/clab/api/global/config/SecurityConfig.java
+++ src/main/java/page/clab/api/global/config/SecurityConfig.java
@@ -196,6 +196,7 @@ public class SecurityConfig {
         DaoAuthenticationProvider loginProvider = new DaoAuthenticationProvider();
         loginProvider.setUserDetailsService(customUserDetailsService);
         loginProvider.setPasswordEncoder(passwordEncoder());
+        loginProvider.setHideUserNotFoundExceptions(false);
         return new ProviderManager(Arrays.asList(loginProvider));
     }
 
