b3aac780635566ae9ad10b5f9d4a379e6ac50113
limehee <noop103@naver.com>
2024-02-29 04:56:42 +0900

    feat(Swagger): Swagger 접속 URL을 변경하며 관련 정보를 숨김

---
+++ src/main/java/page/clab/api/global/config/OpenApiPatternsProperties.java
@@ -0,0 +1,16 @@
+package page.clab.api.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "security.swagger")
+public class OpenApiPatternsProperties {
+
+    private String[] patterns;
+
+}
