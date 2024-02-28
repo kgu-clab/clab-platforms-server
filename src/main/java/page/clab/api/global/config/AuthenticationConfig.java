0f7dc26538e325900d158210b3b19ea10d4f97c5
limehee <noop103@naver.com>
2024-02-29 00:50:35 +0900

    refactor(SecurityConfig): 인증 관련 설정 분리

---
+++ src/main/java/page/clab/api/global/config/AuthenticationConfig.java
@@ -0,0 +1,71 @@
+package page.clab.api.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import page.clab.api.global.auth.application.CustomUserDetailsService;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Value("${security.account.swagger.username}")
+    private String username;
+
+    @Value("${security.account.swagger.password}")
+    private String password;
+
+    @Value("${security.account.swagger.role}")
+    private String role;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles(role)
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean(name = "loginAuthenticationManager")
+    public AuthenticationManager loginAuthenticationManager() {
+        DaoAuthenticationProvider loginProvider = new DaoAuthenticationProvider();
+        loginProvider.setUserDetailsService(customUserDetailsService);
+        loginProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(List.of(loginProvider));
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
