baa36b05024d12fa995f3a4fcea3a73e19cfa94a
limehee <noop103@naver.com>
2024-02-29 21:15:51 +0900

    refactor(SharedAccount): 공동 계정 이용 신청시 startTime이 null일 경우 현재 시간부터 이용하는 것으로 변경

--- src/main/java/page/clab/api/domain/sharedAccount/dto/request/SharedAccountUsageRequestDto.java
+++ src/main/java/page/clab/api/domain/sharedAccount/dto/request/SharedAccountUsageRequestDto.java
@@ -2,12 +2,13 @@ package page.clab.api.domain.sharedAccount.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,7 +21,6 @@ public class SharedAccountUsageRequestDto {
     @Schema(description = "공유 계정 식별 아이디(계정 아이디 X)", example = "1", required = true)
     private Long sharedAccountId;
 
-    @NotNull(message = "{notNull.sharedAccountUsage.startTime}}")
     @Schema(description = "계정 이용 시작 시간", example = "2023-11-06T00:00:00", required = true)
     private LocalDateTime startTime;
 
