81049270e7e3b86b8c2f020ab8912f692fc84f46
limehee <noop103@naver.com>
2024-03-01 01:39:02 +0900

    refactor(FileUpload): 과제 파일 업로드 실패시 상태코드 200과 함께 success: false, data: null을 반환하도록 변경

---
+++ src/main/java/page/clab/api/global/common/file/exception/AssignmentFileUploadFailException.java
@@ -0,0 +1,25 @@
+package page.clab.api.global.common.file.exception;
+
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+
+public class AssignmentFileUploadFailException extends FileUploadException {
+
+    private static final String DEFAULT_MESSAGE = "파일 업로드에 실패했습니다.";
+
+    public AssignmentFileUploadFailException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public AssignmentFileUploadFailException(String msg) {
+        super(msg);
+    }
+
+    public AssignmentFileUploadFailException(Throwable throwable) {
+        super(DEFAULT_MESSAGE, throwable);
+    }
+
+    public AssignmentFileUploadFailException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+
+}
