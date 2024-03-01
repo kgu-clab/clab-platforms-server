2a17198ac62b04733ad71accdcd83ee2ed9bcbba
limehee <noop103@naver.com>
2024-03-01 22:35:44 +0900

    refactor(ActivityGroup): 신청 멤버 상태 변경시 리더일 경우 상태 변경을 못하도록 변경

--- src/main/java/page/clab/api/domain/activityGroup/application/ActivityGroupAdminService.java
+++ src/main/java/page/clab/api/domain/activityGroup/application/ActivityGroupAdminService.java
@@ -20,6 +20,7 @@ import page.clab.api.domain.activityGroup.dto.param.GroupScheduleDto;
 import page.clab.api.domain.activityGroup.dto.request.ActivityGroupRequestDto;
 import page.clab.api.domain.activityGroup.dto.request.ActivityGroupUpdateRequestDto;
 import page.clab.api.domain.activityGroup.dto.response.ActivityGroupMemberWithApplyReasonResponseDto;
+import page.clab.api.domain.activityGroup.exception.LeaderStatusChangeNotAllowedException;
 import page.clab.api.domain.member.application.MemberService;
 import page.clab.api.domain.member.domain.Member;
 import page.clab.api.domain.notification.application.NotificationService;
@@ -166,6 +167,9 @@ public class ActivityGroupAdminService {
         }
         Member member = memberService.getMemberByIdOrThrow(memberId);
         GroupMember groupMember = activityGroupMemberService.getGroupMemberByActivityGroupAndMemberOrThrow(activityGroup, member);
+        if (groupMember.getRole() == ActivityGroupRole.LEADER) {
+            throw new LeaderStatusChangeNotAllowedException("리더의 상태는 변경할 수 없습니다.");
+        }
         groupMember.setStatus(status);
         if (status == GroupMemberStatus.ACCEPTED) {
             groupMember.setRole(ActivityGroupRole.MEMBER);
