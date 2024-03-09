230140402a0a21ea81f47f9ac69d8383a2448dc1
limehee <noop103@naver.com>
2024-03-09 16:12:53 +0900

    refactor(Application): Application 테이블의 PK 변경으로 인한 API 수정

--- src/main/java/page/clab/api/domain/application/dao/ApplicationRepository.java
+++ src/main/java/page/clab/api/domain/application/dao/ApplicationRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import page.clab.api.domain.application.domain.Application;
+import page.clab.api.domain.application.domain.ApplicationId;
 
-public interface ApplicationRepository extends JpaRepository<Application, String> {
+public interface ApplicationRepository extends JpaRepository<Application, ApplicationId> {
 
     Page<Application> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
