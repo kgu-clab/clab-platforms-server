eebaa626664ef91431ef0fcad5b5253d2ed231bc
limehee <noop103@naver.com>
2024-03-11 15:39:22 +0900

    refactor(BookLoanRecord): loanExtensionDate를 dueDate로 이름 변경

--- src/main/java/page/clab/api/domain/book/dao/BookRepository.java
+++ src/main/java/page/clab/api/domain/book/dao/BookRepository.java
@@ -1,12 +1,12 @@
 package page.clab.api.domain.book.dao;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import page.clab.api.domain.book.domain.Book;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Page<Book> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    List<Book> findAllByOrderByCreatedAtDesc();
 
 }
