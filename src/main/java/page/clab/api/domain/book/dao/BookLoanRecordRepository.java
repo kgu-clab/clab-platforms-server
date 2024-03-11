11a07f6ded57c21f610c82ff1fe4497a3f8f4ea5
limehee <noop103@naver.com>
2024-03-11 15:39:22 +0900

    refactor(Book): 도서 조회시 대출 만료 일자를 같이 반환하도록 변경

--- src/main/java/page/clab/api/domain/book/dao/BookLoanRecordRepository.java
+++ src/main/java/page/clab/api/domain/book/dao/BookLoanRecordRepository.java
@@ -1,18 +1,18 @@
 package page.clab.api.domain.book.dao;
 
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import page.clab.api.domain.book.domain.Book;
 import page.clab.api.domain.book.domain.BookLoanRecord;
-import page.clab.api.domain.member.domain.Member;
+
+import java.util.Optional;
 
 public interface BookLoanRecordRepository extends JpaRepository<BookLoanRecord, Long> {
 
     Page<BookLoanRecord> findAllByOrderByBorrowedAtDesc(Pageable pageable);
 
-    Optional<BookLoanRecord> findByBookAndBorrowerAndReturnedAtIsNull(Book book, Member borrower);
+    Optional<BookLoanRecord> findByBookAndReturnedAtIsNull(Book book);
 
     Page<BookLoanRecord> findByBook_IdOrderByBorrowedAtDesc(Long bookId, Pageable pageable);
 
