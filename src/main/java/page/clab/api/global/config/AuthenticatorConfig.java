e6d3f136097e718542a97e885afbdc24ab60d6d4
limehee <noop103@naver.com>
2024-03-18 03:08:02 +0900

    refactor(Authenticator): GoogleAuthenticator를 스프링 빈으로 등록하여 주입받도록 변경

---
+++ src/main/java/page/clab/api/global/config/AuthenticatorConfig.java
@@ -0,0 +1,15 @@
+package page.clab.api.global.config;
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthenticatorConfig {
+
+    @Bean
+    public GoogleAuthenticator googleAuthenticator() {
+        return new GoogleAuthenticator();
+    }
+
+}
