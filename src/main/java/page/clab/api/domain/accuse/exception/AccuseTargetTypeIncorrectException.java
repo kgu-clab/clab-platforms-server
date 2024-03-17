ce1ff06a1d342d7bc49d1f0cc7f66cb94b743277
limehee <noop103@naver.com>
2024-03-17 22:36:49 +0900

    refactor(Accuse): 신고하기 및 신고 상태 변경 API 로직 개선

--- src/main/java/page/clab/api/domain/accuse/exception/AccuseTargetTypeIncorrectException.java
+++ src/main/java/page/clab/api/domain/accuse/exception/AccuseTargetTypeIncorrectException.java
@@ -1,6 +1,6 @@
 package page.clab.api.domain.accuse.exception;
 
-public class AccuseTargetTypeIncorrectException extends RuntimeException{
+public class AccuseTargetTypeIncorrectException extends RuntimeException {
 
     public AccuseTargetTypeIncorrectException(String message) {
             super(message);
