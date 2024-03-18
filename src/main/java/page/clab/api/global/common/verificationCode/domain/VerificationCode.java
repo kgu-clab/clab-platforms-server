94676bdbb571ede9654e890a425287b004c37878
limehee <noop103@naver.com>
2024-03-18 10:14:27 +0900

    refactor(VerificationCode): 일부 로직 도메인 이전

--- src/main/java/page/clab/api/global/common/verificationCode/domain/VerificationCode.java
+++ src/main/java/page/clab/api/global/common/verificationCode/domain/VerificationCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
+import page.clab.api.global.exception.InvalidInformationException;
 
 @Getter
 @Builder
@@ -22,5 +23,22 @@ public class VerificationCode {
 
     @Indexed
     private String verificationCode;
-    
+
+    public static VerificationCode create(String memberId, String verificationCode) {
+        return VerificationCode.builder()
+                .id(memberId)
+                .verificationCode(verificationCode)
+                .build();
+    }
+
+    public boolean isOwner(String memberId) {
+        return this.id.equals(memberId);
+    }
+
+    public void validateRequest(String memberId) {
+        if (!isOwner(memberId)) {
+            throw new InvalidInformationException("올바르지 않은 인증 요청입니다.");
+        }
+    }
+
 }
