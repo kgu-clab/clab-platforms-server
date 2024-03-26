67982a2629a759819b03d94bf817c5a7a0a6d24d
limehee <noop103@naver.com>
2024-03-27 01:58:39 +0900

    refactor(DeleteFileRequestDto): 변수명 복구

--- src/main/java/page/clab/api/global/common/file/dto/request/DeleteFileRequestDto.java
+++ src/main/java/page/clab/api/global/common/file/dto/request/DeleteFileRequestDto.java
@@ -17,11 +17,11 @@ public class DeleteFileRequestDto {
 
     @NotNull(message = "{notNull.file.url}")
     @Schema(description = "파일경로", example = "/resources/files/forms/123456.png", required = true)
-    private String fileUrl;
+    private String url;
 
-    public static DeleteFileRequestDto create(String fileUrl) {
+    public static DeleteFileRequestDto create(String url) {
         return DeleteFileRequestDto.builder()
-                .fileUrl(fileUrl)
+                .url(url)
                 .build();
     }
 
