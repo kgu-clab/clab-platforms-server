3c0bbeb6307de3c54da0a704127eeaa8fb7c70b5
limehee <noop103@naver.com>
2024-04-03 01:33:23 +0900

    refactor(Board): 게시글 목록 조회 반환 정보 추가

--- src/main/java/page/clab/api/domain/board/dto/response/BoardListResponseDto.java
+++ src/main/java/page/clab/api/domain/board/dto/response/BoardListResponseDto.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import page.clab.api.domain.board.domain.Board;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,15 +18,30 @@ public class BoardListResponseDto {
 
     private Long id;
 
+    private String writerId;
+
+    private String writerName;
+
     private String category;
 
     private String title;
 
-    public static BoardListResponseDto toDto(Board board) {
+    private String content;
+
+    private Long commentCount;
+
+    private LocalDateTime createdAt;
+
+    public static BoardListResponseDto toDto(Board board, Long commentCount) {
         return BoardListResponseDto.builder()
                 .id(board.getId())
+                .writerId(board.isWantAnonymous() ? null : board.getMember().getId())
+                .writerName(board.isWantAnonymous() ? board.getNickname() : board.getMember().getName())
                 .category(board.getCategory())
                 .title(board.getTitle())
+                .content(board.getContent())
+                .commentCount(commentCount)
+                .createdAt(board.getCreatedAt())
                 .build();
     }
 
