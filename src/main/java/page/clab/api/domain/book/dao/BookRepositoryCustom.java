1452b607b7feee4f194c4987f0b3b302510bffc7
limehee <noop103@naver.com>
2024-04-02 16:08:47 +0900

    fix(Book): 도서 목록 조회 페이지네이션 누락 수정

--- src/main/java/page/clab/api/domain/book/dao/BookRepositoryCustom.java
+++ src/main/java/page/clab/api/domain/book/dao/BookRepositoryCustom.java
@@ -1,11 +1,12 @@
 package page.clab.api.domain.book.dao;
 
+import org.springframework.data.domain.Pageable;
 import page.clab.api.domain.book.domain.Book;
 
 import java.util.List;
 
 public interface BookRepositoryCustom {
 
-    List<Book> findByConditions(String title, String category, String publisher, String borrowerId, String borrowerName);
+    List<Book> findByConditions(String title, String category, String publisher, String borrowerId, String borrowerName, Pageable pageable);
 
 }
