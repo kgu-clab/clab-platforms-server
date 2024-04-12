680c83d16f8c8cba150bf222f8789b7007ad1920
limehee <noop103@naver.com>
2024-04-13 03:23:00 +0900

    refactor(test): Test 환경 설정

--- src/test/java/page/clab/api/ApiApplicationTests.java
+++ src/test/java/page/clab/api/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package page.clab.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class ApiApplicationTests {
 
 	@Test
