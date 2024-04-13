0dbd033424f6f8203336507816388b9b31c07836
mingmingmon <mingmingmon@naver.com>
2024-04-13 22:50:17 +0900

    fix: 이메일 전송 시 html 파일 경로 지정용 ThymeleafConfig 추가 #299

---
+++ src/main/java/page/clab/api/global/config/ThymeleafConfig.java
@@ -0,0 +1,21 @@
+package page.clab.api.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver() {
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setPrefix(getClass().getResource("/templates/").toString());
+        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
+        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        springResourceTemplateResolver.setCacheable(false);
+        return springResourceTemplateResolver;
+    }
+
+}
