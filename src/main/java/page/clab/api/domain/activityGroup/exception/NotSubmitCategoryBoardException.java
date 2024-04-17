9add7dbca305d8ad26c1d1cfab4e3fe9dc09e2c9
limehee <noop103@naver.com>
2024-04-17 12:38:09 +0900

    refactor(Exception): 미사용 예외 삭제

--- src/main/java/page/clab/api/domain/activityGroup/exception/NotSubmitCategoryBoardException.java
+++
@@ -1,9 +0,0 @@
-package page.clab.api.domain.activityGroup.exception;
-
-public class NotSubmitCategoryBoardException extends RuntimeException {
-
-    public NotSubmitCategoryBoardException(String message) {
-        super(message);
-    }
-
-}
