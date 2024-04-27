ca728fdc8b01995e00ec43fbd0a62d98c5d684fc
mingmingmon <mingmingmon@naver.com>
2024-04-27 21:27:48 +0900

    fix: ActivityGroup 소프트 딜리트 작성 #299

--- src/main/java/page/clab/api/domain/activityGroup/application/ActivityGroupAdminService.java
+++ src/main/java/page/clab/api/domain/activityGroup/application/ActivityGroupAdminService.java
@@ -20,6 +20,9 @@ import page.clab.api.domain.activityGroup.dto.param.GroupScheduleDto;
 import page.clab.api.domain.activityGroup.dto.request.ActivityGroupRequestDto;
 import page.clab.api.domain.activityGroup.dto.request.ActivityGroupUpdateRequestDto;
 import page.clab.api.domain.activityGroup.dto.response.ActivityGroupMemberWithApplyReasonResponseDto;
+import page.clab.api.domain.activityGroup.dto.response.ActivityGroupResponseDto;
+import page.clab.api.domain.award.domain.Award;
+import page.clab.api.domain.award.dto.response.AwardResponseDto;
 import page.clab.api.domain.member.application.MemberService;
 import page.clab.api.domain.member.domain.Member;
 import page.clab.api.domain.notification.application.NotificationService;
@@ -91,11 +94,16 @@ public class ActivityGroupAdminService {
         return activityGroup.getId();
     }
 
+    public PagedResponseDto<ActivityGroupResponseDto> getDeletedActivityGroups(Pageable pageable) {
+        Page<ActivityGroup> activityGroups = activityGroupRepository.findAllByIsDeletedTrue(pageable);
+        return new PagedResponseDto<>(activityGroups.map(ActivityGroupResponseDto::toDto));
+    }
+
     @Transactional
     public Long deleteActivityGroup(Long activityGroupId) {
+        ActivityGroup activityGroup = getActivityGroupByIdOrThrow(activityGroupId);
         List<GroupMember> groupMembers = activityGroupMemberService.getGroupMemberByActivityGroupId(activityGroupId);
         List<GroupSchedule> groupSchedules = groupScheduleRepository.findAllByActivityGroupIdOrderByIdDesc(activityGroupId);
-        ActivityGroup activityGroup = getActivityGroupByIdOrThrow(activityGroupId);
         GroupMember groupLeader = activityGroupMemberService.getGroupMemberByActivityGroupIdAndRole(activityGroupId, ActivityGroupRole.LEADER);
 
         activityGroupMemberService.deleteAll(groupMembers);
