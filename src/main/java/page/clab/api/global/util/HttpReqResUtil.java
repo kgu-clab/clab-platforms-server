545b9b2b601eead592f26f6be604a6575fd08368
한관희 <85067003+limehee@users.noreply.github.com>
2024-06-05 09:27:59 +0900

    IP 지리적 위치 서비스 개선 완료 (#364)
    * refactor(HttpReqResUtil): Bogon IP 판별 로직 개선
    
    * refactor(IpAuthenticationFilter): Bogon IP 판별 로직 중복 제거
    
    * refactor(Filter): 요청 IP에 대한 정보 수집/차단 필터 분리

--- src/main/java/page/clab/api/global/util/HttpReqResUtil.java
+++ src/main/java/page/clab/api/global/util/HttpReqResUtil.java
@@ -1,14 +1,74 @@
 package page.clab.api.global.util;
 
+import io.ipinfo.api.request.IPRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Set;
+import java.util.Arrays;
 
 public class HttpReqResUtil {
 
-    private static final Set<String> localIpSet = Set.of("0:0:0:0:0:0:0:1", "127.0.0.1", "192.168.0.1");
+    private static final IPRequest.IpAddressMatcher[] IpAddressMatcherList = {
+            // IPv4
+            new IPRequest.IpAddressMatcher("0.0.0.0/8"),
+            new IPRequest.IpAddressMatcher("10.0.0.0/8"),
+            new IPRequest.IpAddressMatcher("100.64.0.0/10"),
+            new IPRequest.IpAddressMatcher("127.0.0.0/8"),
+            new IPRequest.IpAddressMatcher("169.254.0.0/16"),
+            new IPRequest.IpAddressMatcher("172.16.0.0/12"),
+            new IPRequest.IpAddressMatcher("192.0.0.0/24"),
+            new IPRequest.IpAddressMatcher("192.0.2.0/24"),
+            new IPRequest.IpAddressMatcher("192.168.0.0/16"),
+            new IPRequest.IpAddressMatcher("198.18.0.0/15"),
+            new IPRequest.IpAddressMatcher("198.51.100.0/24"),
+            new IPRequest.IpAddressMatcher("203.0.113.0/24"),
+            new IPRequest.IpAddressMatcher("224.0.0.0/4"),
+            new IPRequest.IpAddressMatcher("240.0.0.0/4"),
+            new IPRequest.IpAddressMatcher("255.255.255.255/32"),
+            // IPv6
+            new IPRequest.IpAddressMatcher("::/128"),
+            new IPRequest.IpAddressMatcher("::1/128"),
+            new IPRequest.IpAddressMatcher("::ffff:0:0/96"),
+            new IPRequest.IpAddressMatcher("::/96"),
+            new IPRequest.IpAddressMatcher("100::/64"),
+            new IPRequest.IpAddressMatcher("2001:10::/28"),
+            new IPRequest.IpAddressMatcher("2001:db8::/32"),
+            new IPRequest.IpAddressMatcher("fc00::/7"),
+            new IPRequest.IpAddressMatcher("fe80::/10"),
+            new IPRequest.IpAddressMatcher("fec0::/10"),
+            new IPRequest.IpAddressMatcher("ff00::/8"),
+            // 6to4
+            new IPRequest.IpAddressMatcher("2002::/24"),
+            new IPRequest.IpAddressMatcher("2002:a00::/24"),
+            new IPRequest.IpAddressMatcher("2002:7f00::/24"),
+            new IPRequest.IpAddressMatcher("2002:a9fe::/32"),
+            new IPRequest.IpAddressMatcher("2002:ac10::/28"),
+            new IPRequest.IpAddressMatcher("2002:c000::/40"),
+            new IPRequest.IpAddressMatcher("2002:c000:200::/40"),
+            new IPRequest.IpAddressMatcher("2002:c0a8::/32"),
+            new IPRequest.IpAddressMatcher("2002:c612::/31"),
+            new IPRequest.IpAddressMatcher("2002:c633:6400::/40"),
+            new IPRequest.IpAddressMatcher("2002:cb00:7100::/40"),
+            new IPRequest.IpAddressMatcher("2002:e000::/20"),
+            new IPRequest.IpAddressMatcher("2002:f000::/20"),
+            new IPRequest.IpAddressMatcher("2002:ffff:ffff::/48"),
+            // Teredo
+            new IPRequest.IpAddressMatcher("2001::/40"),
+            new IPRequest.IpAddressMatcher("2001:0:a00::/40"),
+            new IPRequest.IpAddressMatcher("2001:0:7f00::/40"),
+            new IPRequest.IpAddressMatcher("2001:0:a9fe::/48"),
+            new IPRequest.IpAddressMatcher("2001:0:ac10::/44"),
+            new IPRequest.IpAddressMatcher("2001:0:c000::/56"),
+            new IPRequest.IpAddressMatcher("2001:0:c000:200::/56"),
+            new IPRequest.IpAddressMatcher("2001:0:c0a8::/48"),
+            new IPRequest.IpAddressMatcher("2001:0:c612::/47"),
+            new IPRequest.IpAddressMatcher("2001:0:c633:6400::/56"),
+            new IPRequest.IpAddressMatcher("2001:0:cb00:7100::/56"),
+            new IPRequest.IpAddressMatcher("2001:0:e000::/36"),
+            new IPRequest.IpAddressMatcher("2001:0:f000::/36"),
+            new IPRequest.IpAddressMatcher("2001:0:ffff:ffff::/64")
+    };
 
     private static final String[] IP_HEADER_CANDIDATES = {
             "X-Forwarded-For",
@@ -38,8 +98,9 @@ public class HttpReqResUtil {
         return request.getRemoteAddr();
     }
 
-    public static boolean isLocalRequest(String ipAddress) {
-        return localIpSet.contains(ipAddress);
+    public static boolean isBogonRequest(String ip) {
+        return Arrays.stream(IpAddressMatcherList)
+                .anyMatch(ipAddressMatcher -> ipAddressMatcher.matches(ip));
     }
 
 }
