6a8bb58e213e7d2044defb89d616c067c97eac63
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-06-25 00:29:42 +0900

    feat: 모집 기간에 따라 자동으로 모집 상태 변경하는 로직 작성 #353

--- src/main/java/page/clab/api/domain/recruitment/application/RecruitmentService.java
+++ src/main/java/page/clab/api/domain/recruitment/application/RecruitmentService.java
@@ -1,13 +1,19 @@
 package page.clab.api.domain.recruitment.application;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import page.clab.api.domain.notification.application.NotificationService;
 import page.clab.api.domain.recruitment.dao.RecruitmentRepository;
 import page.clab.api.domain.recruitment.domain.Recruitment;
+import page.clab.api.domain.recruitment.domain.RecruitmentStatus;
 import page.clab.api.domain.recruitment.dto.request.RecruitmentRequestDto;
 import page.clab.api.domain.recruitment.dto.request.RecruitmentUpdateRequestDto;
 import page.clab.api.domain.recruitment.dto.response.RecruitmentResponseDto;
@@ -15,6 +21,7 @@ import page.clab.api.global.common.dto.PagedResponseDto;
 import page.clab.api.global.exception.NotFoundException;
 import page.clab.api.global.validation.ValidationService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -27,9 +34,16 @@ public class RecruitmentService {
 
     private final RecruitmentRepository recruitmentRepository;
 
+    private final PlatformTransactionManager transactionManager;
+
+    private final EntityManager entityManager;
+
+    private final TransactionDefinition transactionDefinition;
+
     @Transactional
     public Long createRecruitment(RecruitmentRequestDto requestDto) {
         Recruitment recruitment = RecruitmentRequestDto.toEntity(requestDto);
+        updateRecruitmentStatusByRecruitment(recruitment);
         validationService.checkValid(recruitment);
         notificationService.sendNotificationToAllMembers("새로운 모집 공고가 등록되었습니다.");
         return recruitmentRepository.save(recruitment).getId();
@@ -67,6 +81,23 @@ public class RecruitmentService {
     public Recruitment getRecruitmentByIdOrThrow(Long recruitmentId) {
         return recruitmentRepository.findById(recruitmentId)
                 .orElseThrow(() -> new NotFoundException("해당 모집 공고가 존재하지 않습니다."));
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void updateRecruitmentStatus(){
+        List<Recruitment> recruitments = recruitmentRepository.findAll();
+        recruitments.forEach(this::updateRecruitmentStatusByRecruitment);
+    }
+
+    public void updateRecruitmentStatusByRecruitment(Recruitment recruitment){
+        TransactionStatus transactionStatus = transactionManager.getTransaction(transactionDefinition);
+        LocalDateTime now = LocalDateTime.now();
+        RecruitmentStatus newStatus = now.isBefore(recruitment.getStartDate())
+                ? RecruitmentStatus.UPCOMING : now.isAfter(recruitment.getEndDate())
+                    ? RecruitmentStatus.CLOSED : RecruitmentStatus.OPEN;
+        recruitment.updateStatus(newStatus);
+        entityManager.merge(recruitment);
+        transactionManager.commit(transactionStatus);
     }
 
 }
