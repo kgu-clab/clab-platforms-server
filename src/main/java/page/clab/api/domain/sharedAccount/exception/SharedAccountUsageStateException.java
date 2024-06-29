80b0ead80d47a16f67d765f46d73bfc2cbb6acfc
한관희 <noop103@naver.com>
2024-06-29 16:56:43 +0900

    refactor(SharedAccountUsage): 미사용 기능 제거

--- src/main/java/page/clab/api/domain/sharedAccount/exception/SharedAccountUsageStateException.java
+++
@@ -1,9 +0,0 @@
-package page.clab.api.domain.sharedAccount.exception;
-
-public class SharedAccountUsageStateException extends RuntimeException {
-
-    public SharedAccountUsageStateException(String message) {
-        super(message);
-    }
-
-}
