0bf2e7c74fee659e78375c3a977fdd8f6f0a2500
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-07-03 09:37:11 +0900

    feat(GeneralAlertType): enum 필드 추가

--- src/main/java/page/clab/api/global/common/slack/domain/GeneralAlertType.java
+++ src/main/java/page/clab/api/global/common/slack/domain/GeneralAlertType.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum GeneralAlertType implements AlertType {
 
     ADMIN_LOGIN("관리자 로그인", "Admin login."),
+    SWAGGER_ACCESS("Swagger 접근", "Swagger accessed."),
     APPLICATION_CREATED("새 지원서", "New application has been submitted."),
     BOARD_CREATED("새 게시글", "New board has been created."),
     SERVER_START("서버 시작", "Server has been started."),
