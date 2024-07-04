0bcae68a673e58d6b7f303199de0c68cf1f48f02
한관희 <noop103@naver.com>
2024-07-04 18:10:30 +0900

    refactor(OrderSpecifierUtil): 가독성 향상

--- src/main/java/page/clab/api/global/util/OrderSpecifierUtil.java
+++ src/main/java/page/clab/api/global/util/OrderSpecifierUtil.java
@@ -5,6 +5,7 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.PathBuilder;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,12 +13,14 @@ public class OrderSpecifierUtil {
     
     public static OrderSpecifier<?>[] getOrderSpecifiers(Pageable pageable, EntityPathBase<?> q) {
         return pageable.getSort().stream()
-                .map(order -> {
-                    Order direction = order.isAscending() ? Order.ASC : Order.DESC;
-                    String property = order.getProperty();
-                    PathBuilder<Object> path = new PathBuilder<>(q.getType(), q.getMetadata());
-                    return new OrderSpecifier(direction, path.get(property));
-                })
+                .map(order -> createOrderSpecifier(order, q))
                 .toArray(OrderSpecifier[]::new);
+    }
+    
+    private static OrderSpecifier<?> createOrderSpecifier(Sort.Order order, EntityPathBase<?> q) {
+        Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+        String property = order.getProperty();
+        PathBuilder<Object> path = new PathBuilder<>(q.getType(), q.getMetadata());
+        return new OrderSpecifier(direction, path.get(property));
     }
 }
