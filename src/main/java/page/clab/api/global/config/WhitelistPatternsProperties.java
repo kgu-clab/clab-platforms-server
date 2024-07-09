229e0956dfa8b4761619f145e52e36953ad7a8f5
한관희 <noop103@naver.com>
2024-07-10 02:26:52 +0900

    스쿼시 머지: feat/#397

--- src/main/java/page/clab/api/global/config/WhitelistPatternsProperties.java
+++ src/main/java/page/clab/api/global/config/WhitelistPatternsProperties.java
@@ -5,12 +5,21 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 @Setter
 @Getter
 @Configuration
-@ConfigurationProperties(prefix = "security.whitelist")
+@ConfigurationProperties(prefix = "security.whitelist.patterns")
 public class WhitelistPatternsProperties {
 
-    private String[] patterns;
+    private String[] actuator;
+    private String[] apiDocs;
 
+    public String[] getWhitelistPatterns() {
+        return Stream.of(apiDocs, actuator)
+                .flatMap(Arrays::stream)
+                .toArray(String[]::new);
+    }
 }
