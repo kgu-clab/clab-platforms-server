194502bdd8e67ebc5a0da17d28d90109f2a1bdff
한관희 <noop103@naver.com>
2024-07-18 22:09:29 +0900

    refactor(SRP): 스케줄링, JPAAuditing 활성화 어노테이션을 Config로 분리

--- src/main/java/page/clab/api/global/config/SchedulerConfig.java
+++ src/main/java/page/clab/api/global/config/SchedulerConfig.java
@@ -1,11 +1,13 @@
 package page.clab.api.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
+@EnableScheduling
 class SchedulerConfig implements SchedulingConfigurer {
 
     @Override
