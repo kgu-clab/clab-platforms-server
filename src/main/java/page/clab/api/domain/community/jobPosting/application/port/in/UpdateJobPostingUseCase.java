6edaee2c92c346058b155ea4bec471422a3c34fc
한관희 <noop103@naver.com>
2024-08-03 11:53:00 +0900

    refactor(JobPosting): 미사용 도메인 삭제

--- src/main/java/page/clab/api/domain/community/jobPosting/application/port/in/UpdateJobPostingUseCase.java
+++
@@ -1,7 +0,0 @@
-package page.clab.api.domain.community.jobPosting.application.port.in;
-
-import page.clab.api.domain.community.jobPosting.application.dto.request.JobPostingUpdateRequestDto;
-
-public interface UpdateJobPostingUseCase {
-    Long updateJobPosting(Long jobPostingId, JobPostingUpdateRequestDto requestDto);
-}
