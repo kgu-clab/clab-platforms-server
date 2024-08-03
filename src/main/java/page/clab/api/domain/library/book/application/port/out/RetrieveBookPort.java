032a328a129145afcdbb2386f90a28d2ec49b41c
한관희 <noop103@naver.com>
2024-08-03 11:27:49 +0900

    refactor(Library): 도서 관련 미사용 API 삭제

--- src/main/java/page/clab/api/domain/library/book/application/port/out/RetrieveBookPort.java
+++ src/main/java/page/clab/api/domain/library/book/application/port/out/RetrieveBookPort.java
@@ -8,8 +8,6 @@ public interface RetrieveBookPort {
 
     Book findByIdOrThrow(Long bookId);
 
-    Page<Book> findAllByIsDeletedTrue(Pageable pageable);
-
     Page<Book> findByConditions(String title, String category, String publisher, String borrowerId, String borrowerName, Pageable pageable);
 
     int countByBorrowerId(String borrowerId);
