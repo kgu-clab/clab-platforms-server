86550eacdff76a4e0bd5b5b1fe843f5d4553fbb0
한관희 <noop103@naver.com>
2024-08-03 11:42:47 +0900

    refactor(Members): 일정 관련 미사용 API 삭제

--- src/main/java/page/clab/api/domain/members/schedule/adapter/out/persistence/ScheduleRepository.java
+++ src/main/java/page/clab/api/domain/members/schedule/adapter/out/persistence/ScheduleRepository.java
@@ -1,13 +1,7 @@
 package page.clab.api.domain.members.schedule.adapter.out.persistence;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface ScheduleRepository extends JpaRepository<ScheduleJpaEntity, Long>, ScheduleRepositoryCustom, QuerydslPredicateExecutor<ScheduleJpaEntity> {
-
-    @Query(value = "SELECT s.* FROM schedule s WHERE s.is_deleted = true", nativeQuery = true)
-    Page<ScheduleJpaEntity> findAllByIsDeletedTrue(Pageable pageable);
 }
