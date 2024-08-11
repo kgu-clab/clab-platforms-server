26fd94e5d821ff8b80e0ed9128fc13fdd2f8a100
mingmingmon <mingmingmon@naver.com>
2024-08-11 15:00:07 +0900

    refactor(File): 작업과 관련 없는 코드 롤백 #447

--- src/main/java/page/clab/api/global/common/file/api/FileController.java
+++ src/main/java/page/clab/api/global/common/file/api/FileController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import page.clab.api.global.common.dto.ApiResponse;
