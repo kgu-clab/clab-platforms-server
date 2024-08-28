ff76f98623fa3390acd9b05c8db10e040b2d8535
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-28 21:26:57 +0900

    refactor(ApplicationEvent): ApplicationApprovedEvent -> ApplicationMemberCreatedEvent

--- src/main/java/page/clab/api/domain/hiring/application/application/event/ApplicationMemberCreatedEvent.java
+++ src/main/java/page/clab/api/domain/hiring/application/application/event/ApplicationMemberCreatedEvent.java
@@ -5,11 +5,11 @@ import org.springframework.context.ApplicationEvent;
 import page.clab.api.domain.hiring.application.domain.Application;
 
 @Getter
-public class ApplicationApprovedEvent extends ApplicationEvent {
+public class ApplicationMemberCreatedEvent extends ApplicationEvent {
 
     private final Application application;
 
-    public ApplicationApprovedEvent(Object source, Application application) {
+    public ApplicationMemberCreatedEvent(Object source, Application application) {
         super(source);
         this.application = application;
     }
