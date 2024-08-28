23c8340e0ef68519dff2d8d737d4695502994c75
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-28 21:23:19 +0900

    refactor(MemberEventDispatcher): 불필요한 import문 삭제

--- src/main/java/page/clab/api/domain/memberManagement/member/application/event/MemberEventDispatcher.java
+++ src/main/java/page/clab/api/domain/memberManagement/member/application/event/MemberEventDispatcher.java
@@ -3,9 +3,6 @@ package page.clab.api.domain.memberManagement.member.application.event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import page.clab.api.domain.hiring.application.application.event.ApplicationApprovedEvent;
-import page.clab.api.domain.hiring.application.domain.Application;
-import page.clab.api.domain.memberManagement.member.domain.Member;
 
 import java.util.List;
 
