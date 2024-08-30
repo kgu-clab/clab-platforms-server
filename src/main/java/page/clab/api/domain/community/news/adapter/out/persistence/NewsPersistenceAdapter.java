095cc6a71aaebb40d7328151a3dc2b01619284db
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-30 18:28:25 +0900

    refactor(News): Mapper 메서드명 변경(toEntity, toDomain)

--- src/main/java/page/clab/api/domain/community/news/adapter/out/persistence/NewsPersistenceAdapter.java
+++ src/main/java/page/clab/api/domain/community/news/adapter/out/persistence/NewsPersistenceAdapter.java
@@ -19,13 +19,13 @@ public class NewsPersistenceAdapter implements
     @Override
     public News getById(Long id) {
         return repository.findById(id)
-                .map(mapper::toDomainEntity)
+                .map(mapper::toDomain)
                 .orElseThrow(() -> new NotFoundException("[News] id: " + id + "에 해당하는 뉴스가 존재하지 않습니다."));
     }
 
     @Override
     public Page<News> findByConditions(String title, String category, Pageable pageable) {
         return repository.findByConditions(title, category, pageable)
-                .map(mapper::toDomainEntity);
+                .map(mapper::toDomain);
     }
 }
