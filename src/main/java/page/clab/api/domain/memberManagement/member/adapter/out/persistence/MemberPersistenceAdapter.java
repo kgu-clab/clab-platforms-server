677bf4a001332f0f43615511c9c4a4949ec6a433
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-08-30 18:29:56 +0900

    refactor(Member): Mapper 메서드명 변경(toEntity, toDomain)

--- src/main/java/page/clab/api/domain/memberManagement/member/adapter/out/persistence/MemberPersistenceAdapter.java
+++ src/main/java/page/clab/api/domain/memberManagement/member/adapter/out/persistence/MemberPersistenceAdapter.java
@@ -43,72 +43,72 @@ public class MemberPersistenceAdapter implements
 
     @Override
     public Member save(Member member) {
-        MemberJpaEntity jpaEntity = memberMapper.toJpaEntity(member);
+        MemberJpaEntity jpaEntity = memberMapper.toEntity(member);
         MemberJpaEntity savedEntity = memberRepository.save(jpaEntity);
-        return memberMapper.toDomainEntity(savedEntity);
+        return memberMapper.toDomain(savedEntity);
     }
 
     @Override
     public Member update(Member member) {
-        MemberJpaEntity jpaEntity = memberMapper.toJpaEntity(member);
+        MemberJpaEntity jpaEntity = memberMapper.toEntity(member);
         MemberJpaEntity updatedEntity = memberRepository.save(jpaEntity);
-        return memberMapper.toDomainEntity(updatedEntity);
+        return memberMapper.toDomain(updatedEntity);
     }
 
     @Override
     public Page<Member> findByConditions(String id, String name, Pageable pageable) {
         Page<MemberJpaEntity> jpaEntities = memberRepository.findByConditions(id, name, pageable);
-        return jpaEntities.map(memberMapper::toDomainEntity);
+        return jpaEntities.map(memberMapper::toDomain);
     }
 
     @Override
     public Member getFirstByRole(Role role) {
         MemberJpaEntity jpaEntity = memberRepository.findFirstByRole(role)
                 .orElseThrow(() -> new NotFoundException("[Member] role: " + role + "에 해당하는 회원이 존재하지 않습니다."));
-        return memberMapper.toDomainEntity(jpaEntity);
+        return memberMapper.toDomain(jpaEntity);
     }
 
     @Override
     public Optional<Member> findById(String memberId) {
-        return memberRepository.findById(memberId).map(memberMapper::toDomainEntity);
+        return memberRepository.findById(memberId).map(memberMapper::toDomain);
     }
 
     @Override
     public Member getById(String memberId) {
         MemberJpaEntity jpaEntity = memberRepository.findById(memberId)
                 .orElseThrow(() -> new NotFoundException("[Member] id: " + memberId + "에 해당하는 회원이 존재하지 않습니다."));
-        return memberMapper.toDomainEntity(jpaEntity);
+        return memberMapper.toDomain(jpaEntity);
     }
 
     @Override
     public List<Member> findAll() {
         List<MemberJpaEntity> jpaEntities = memberRepository.findAll();
         return jpaEntities.stream()
-                .map(memberMapper::toDomainEntity)
+                .map(memberMapper::toDomain)
                 .toList();
     }
 
     public Page<Member> findMemberRoleInfoByConditions(String memberId, String memberName, Role role, Pageable pageable) {
         Page<MemberJpaEntity> jpaEntities = memberRepository.findMemberRoleInfoByConditions(memberId, memberName, role, pageable);
-        return jpaEntities.map(memberMapper::toDomainEntity);
+        return jpaEntities.map(memberMapper::toDomain);
     }
 
     @Override
     public Page<Member> findAllByOrderByCreatedAtDesc(Pageable pageable) {
         Page<MemberJpaEntity> jpaEntities = memberRepository.findAllByOrderByCreatedAtDesc(pageable);
-        return jpaEntities.map(memberMapper::toDomainEntity);
+        return jpaEntities.map(memberMapper::toDomain);
     }
 
     @Override
     public Member getByEmail(String email) {
         MemberJpaEntity jpaEntity = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new NotFoundException("[Member] email: " + email + "을 사용하는 회원이 존재하지 않습니다."));
-        return memberMapper.toDomainEntity(jpaEntity);
+        return memberMapper.toDomain(jpaEntity);
     }
 
     @Override
     public Page<Member> findBirthdaysThisMonth(int month, Pageable pageable) {
         Page<MemberJpaEntity> jpaEntities = memberRepository.findBirthdaysThisMonth(month, pageable);
-        return jpaEntities.map(memberMapper::toDomainEntity);
+        return jpaEntities.map(memberMapper::toDomain);
     }
 }
