9c7795d7988c8cc30baf98681ee885891f7b67f2
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-09-10 22:22:06 +0900

    refactor(Member): develop rebase에 따른 변경사항 수정

--- src/main/java/page/clab/api/domain/memberManagement/member/application/service/MemberRegisterService.java
+++ src/main/java/page/clab/api/domain/memberManagement/member/application/service/MemberRegisterService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import page.clab.api.domain.memberManagement.member.application.dto.mapper.MemberDtoMapper;
 import page.clab.api.domain.memberManagement.member.application.dto.request.MemberRequestDto;
 import page.clab.api.domain.memberManagement.member.application.exception.DuplicateMemberContactException;
 import page.clab.api.domain.memberManagement.member.application.exception.DuplicateMemberEmailException;
@@ -37,7 +38,7 @@ public class MemberRegisterService implements RegisterMemberUseCase {
     @Override
     public String registerMember(MemberRequestDto requestDto) {
         checkMemberUniqueness(requestDto);
-        Member member = MemberRequestDto.toEntity(requestDto);
+        Member member = MemberDtoMapper.toMember(requestDto);
         String finalPassword = manageMemberPasswordUseCase.generateOrRetrievePassword(requestDto.getPassword());
         member.updatePassword(finalPassword, passwordEncoder);
         registerMemberPort.save(member);
