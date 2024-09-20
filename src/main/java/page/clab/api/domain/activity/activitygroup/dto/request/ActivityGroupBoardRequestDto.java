6966cd31abb310cf0bd297fd3c867ba8cb8a6fe0
mingmingmon <mingmingmon@naver.com>
2024-09-20 18:33:52 +0900

    refactor(ActivityGroupBoardRequestDto): 마감 기한이 현재시각보다 이전인 경우에 대한 validation 어노테이션 추가

--- src/main/java/page/clab/api/domain/activity/activitygroup/dto/request/ActivityGroupBoardRequestDto.java
+++ src/main/java/page/clab/api/domain/activity/activitygroup/dto/request/ActivityGroupBoardRequestDto.java
@@ -1,6 +1,7 @@
 package page.clab.api.domain.activity.activitygroup.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,6 +31,7 @@ public class ActivityGroupBoardRequestDto {
     @Schema(description = "첨부파일 경로 리스트", example = "[\"/resources/files/assignment/1/1/superuser/339609571877700_4305d83e-090a-480b-a470-b5e96164d113.png\", \"/resources/files/assignment/2/1/superuser/4305d83e-090a-480b-a470-b5e96164d114.png\"]")
     private List<String> fileUrls;
 
+    @FutureOrPresent(message = "{invalid.activityGroupBoard.dueDateTime}")
     @Schema(description = "마감일자", example = "2024-11-28 18:00:00.000")
     private LocalDateTime dueDateTime;
 
