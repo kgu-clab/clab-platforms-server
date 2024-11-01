95090b8c79f0b16daad7742486ba72042a3fe3e0
SongJaeHoonn <thdwogns121@naver.com>
2024-11-01 17:02:57 +0900

    refactor(BlacklistIp): 주석 및 JavaDocs 추가

--- src/main/java/page/clab/api/domain/auth/blacklistIp/application/service/BlacklistIpRegisterService.java
+++ src/main/java/page/clab/api/domain/auth/blacklistIp/application/service/BlacklistIpRegisterService.java
@@ -22,6 +22,17 @@ public class BlacklistIpRegisterService implements RegisterBlacklistIpUseCase {
     private final SlackService slackService;
     private final BlacklistIpDtoMapper mapper;
 
+    /**
+     * 지정된 IP 주소를 블랙리스트에 등록합니다.
+     *
+     * <p>해당 IP 주소가 이미 블랙리스트에 존재하는지 확인하고,
+     * 존재하지 않을 경우 새롭게 등록합니다.
+     * 새로운 IP가 등록되면 Slack을 통해 보안 알림이 전송됩니다.</p>
+     *
+     * @param request 현재 요청 객체
+     * @param requestDto 블랙리스트에 추가할 IP 주소 정보를 담은 DTO
+     * @return 기존에 존재하거나 새로 추가된 블랙리스트 IP 주소
+     */
     @Transactional
     @Override
     public String registerBlacklistIp(HttpServletRequest request, BlacklistIpRequestDto requestDto) {
