f4d5a1966180d17bb4257a1ae35e959629eb1871
한관희 <noop103@naver.com>
2024-11-14 00:33:57 +0900

    feat(Dependency): 디스코드 웹훅 연결을 위한 OpenFeign 의존성 추가

--- src/main/java/page/clab/api/ApiApplication.java
+++ src/main/java/page/clab/api/ApiApplication.java
@@ -2,7 +2,9 @@ package page.clab.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiApplication {
 
