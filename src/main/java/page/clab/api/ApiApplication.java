a1fda4923943fdb9d410ba67c5ec4fb9fb4b8ead
한관희 <noop103@naver.com>
2024-11-14 14:45:55 +0900

    refactor(Dependency): OpenFeign 의존성 제거

--- src/main/java/page/clab/api/ApiApplication.java
+++ src/main/java/page/clab/api/ApiApplication.java
@@ -2,9 +2,7 @@ package page.clab.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class ApiApplication {
 
