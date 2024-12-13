fe8ed1c524ce7c498ffa9a2098763d249f07914b
SongJaeHoonn <thdwogns121@naver.com>
2024-12-13 13:52:09 +0900

    refactor(RetrieveHotBoardPort): 인기게시글 조회시 전략 이름을 사용하도록 변경

--- src/main/java/page/clab/api/domain/community/board/application/port/out/RetrieveHotBoardPort.java
+++ src/main/java/page/clab/api/domain/community/board/application/port/out/RetrieveHotBoardPort.java
@@ -3,5 +3,5 @@ package page.clab.api.domain.community.board.application.port.out;
 import java.util.List;
 
 public interface RetrieveHotBoardPort {
-    List<String> findAll();
+    List<String> findByHotBoardStrategy(String strategyName);
 }
