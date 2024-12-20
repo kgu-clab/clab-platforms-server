5c93444661c96795781bf06549b1421034c6a58c
mingmingmon <mingmingmon@naver.com>
2024-12-20 17:50:14 +0900

    refactor(BoardEmojiDtoMapper): 불필요한 어노테이션 삭제

--- src/main/java/page/clab/api/domain/community/board/application/dto/mapper/BoardEmojiDtoMapper.java
+++ src/main/java/page/clab/api/domain/community/board/application/dto/mapper/BoardEmojiDtoMapper.java
@@ -1,12 +1,10 @@
 package page.clab.api.domain.community.board.application.dto.mapper;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import page.clab.api.domain.community.board.application.dto.response.BoardEmojiToggleResponseDto;
 import page.clab.api.domain.community.board.domain.BoardEmoji;
 
 @Component
-@RequiredArgsConstructor
 public class BoardEmojiDtoMapper {
 
     public BoardEmojiToggleResponseDto toDto(BoardEmoji boardEmoji) {
