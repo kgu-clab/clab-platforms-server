42b747d79086656e1afd7fd955c47e44758a74fc
mingmingmon <mingmingmon@naver.com>
2024-12-28 17:28:47 +0900

    refactor(HashtagRequestDto): hashtag category notnull 어노테이션 추가

--- src/main/java/page/clab/api/domain/community/hashtag/application/dto/request/HashtagRequestDto.java
+++ src/main/java/page/clab/api/domain/community/hashtag/application/dto/request/HashtagRequestDto.java
@@ -1,5 +1,6 @@
 package page.clab.api.domain.community.hashtag.application.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import page.clab.api.domain.community.hashtag.domain.HashtagCategory;
@@ -9,5 +10,7 @@ import page.clab.api.domain.community.hashtag.domain.HashtagCategory;
 public class HashtagRequestDto {
 
     private String name;
+
+    @NotNull
     private HashtagCategory hashtagCategory;
 }
