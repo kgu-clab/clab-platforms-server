2ef9189c3196e08fac2b94b98042486e38a0a25a
JangYeongHu <jyhoo040912@gmail.com>
2025-06-29 22:26:59 +0900

    refactor(Support): 의미 없는 Enumrated 삭제

--- src/main/java/page/clab/api/domain/members/support/domain/SupportCategory.java
+++ src/main/java/page/clab/api/domain/members/support/domain/SupportCategory.java
@@ -1,7 +1,5 @@
 package page.clab.api.domain.members.support.domain;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +9,6 @@ public enum SupportCategory {
     BUG("BUG", "버그"),
     INQUIRY("INQUIRY", "문의");
 
-    @Enumerated(EnumType.STRING)
     private final String key;
     private final String description;
 }
--- src/main/java/page/clab/api/domain/members/support/domain/SupportStatus.java
+++ src/main/java/page/clab/api/domain/members/support/domain/SupportStatus.java
@@ -1,7 +1,5 @@
 package page.clab.api.domain.members.support.domain;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,7 +10,6 @@ public enum SupportStatus {
     PENDING("PENDING", "처리 전"),
     COMPLETED("COMPLETED", "처리 완료");
 
-    @Enumerated(EnumType.STRING)
     private final String key;
     private final String description;
 }
